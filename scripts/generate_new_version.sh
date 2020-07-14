ff3e2572afdf0e2005eb50b434d265d63f04cfa5
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2020-07-14 11:10:52 -0300

    DSO-33 - Adding script to rename, copy and commit new tokens files

---
+++ scripts/generate_new_version.sh
@@ -0,0 +1,38 @@
+LATEST_VERSION_TOKENS_LIB_NPM=`npm show @useblu/tokens version`
+
+echo '\033[1;31m Downloading tokens into npm lib…\033[0m'
+npm install @useblu/tokens@beta --save-dev
+
+# echo '\033[1;31m Copying resource XML file \033[0m'
+cp node_modules/@useblu/tokens/dist/android/tokens.xml ocean-ds-tokens/src/main/res/values/tokens.xml
+
+# echo '\033[1;31m Copying all font files \033[0m'
+cp node_modules/@useblu/tokens/dist/assets/fonts/*/*.ttf ocean-ds-tokens/src/main/res/font
+
+cd ocean-ds-tokens/src/main/res/font
+
+# echo '\033[1;31m Renaming all font files \033[0m'
+for f in *.ttf
+do
+	mv $f $(echo $f |
+	sed -e 's/Avenir/font_family_highlight/g' |
+	sed -e 's/NunitoSans/font_family_base/g' |
+	sed -e 's/-/_/g' |
+	sed -e 's/Roman/Regular/g' |
+	sed -e 's/SemiBold/Medium/g' |
+	sed -e 's/Heavy/Bold/g' |
+  sed -e 's/Black/Extrabold/g' |
+	tr '[:upper:]' '[:lower:]')
+done
+
+cd ../../../../..
+
+pwd
+
+echo '\033[1;31m Show git status \033[0m'
+git status
+
+echo '\033[1;31m Add and Commit new classes \033[0m'
+#git add . #&& git commit -am "Generating classes based on @useblu/tokens npm lib version: $LATEST_VERSION_TOKENS_LIB_NPM"
+
+

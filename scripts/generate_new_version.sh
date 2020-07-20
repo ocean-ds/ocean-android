2f0d1ac199c9da4ecb122728bb4f926c0cc7027d
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2020-07-20 10:38:12 -0300

    DSO-33 - Fixing CI config

--- scripts/generate_new_version.sh
+++ scripts/generate_new_version.sh
@@ -1,23 +1,29 @@
-LATEST_VERSION_TOKENS_LIB_NPM=`npm show @useblu/tokens@beta version`
+set -e
 
-echo '\033[1;31m Getting new version and saving on version.properties file. \033[0m'
+LATEST_VERSION_TOKENS_LIB_NPM=`npm show @useblu/tokens@beta version`
+echo "\033[1;31m >>>> Current version is '$LATEST_VERSION_TOKENS_LIB_NPM'. \033[0m"
+
+echo '\033[1;31m >>>> Getting new version and saving on version.properties file. \033[0m'
 sed -i-e "/VERSION_NAME=/ s/=.*/=$LATEST_VERSION_TOKENS_LIB_NPM/g" "ocean-ds-components/version.properties"
 
-echo '\033[1;31m Deleting unnecessary temp file. \033[0m'
+echo '\033[1;31m >>>> Deleting unnecessary temp file. \033[0m'
 rm -f "ocean-ds-components/version.properties-e"
 
-echo '\033[1;31m Downloading tokens into npm lib…\033[0m'
+echo '\033[1;31m >>>> What is the current DIR?\033[0m'
+pwd
+
+echo '\033[1;31m >>>> Downloading tokens into npm lib…\033[0m'
 npm install @useblu/tokens@beta --save-dev
 
-# echo '\033[1;31m Copying resource XML file \033[0m'
+echo '\033[1;31m >>>> Copying resources XML file \033[0m'
 cp node_modules/@useblu/tokens/dist/android/tokens.xml ocean-ds-tokens/src/main/res/values/tokens.xml
 
-# echo '\033[1;31m Copying all font files \033[0m'
+echo '\033[1;31m >>>> Copying all font files \033[0m'
 cp node_modules/@useblu/tokens/dist/assets/fonts/*/*.ttf ocean-ds-tokens/src/main/res/font
 
 cd ocean-ds-tokens/src/main/res/font
 
-# echo '\033[1;31m Renaming all font files \033[0m'
+echo '\033[1;31m >>>> Renaming all font files \033[0m'
 for f in *.ttf
 do
 	mv $f $(echo $f |
@@ -35,16 +41,14 @@ cd ../../../../..
 
 pwd
 
-echo '\033[1;31m Show git status \033[0m'
+echo '\033[1;31m >>>> Show git status \033[0m'
 git status
 
-echo '\033[1;31m Show git status \033[0m'
+echo '\033[1;31m >>>> Running gradlew build \033[0m'
 ./gradlew clean assembleRelease publish --stacktrace --info --debug
 
-echo '\033[1;31m Add and Commit new classes \033[0m'
+echo '\033[1;31m >>>> Add and Commit new classes \033[0m'
 git add . && git commit -am "Generating classes based on @useblu/tokens npm lib version: $LATEST_VERSION_TOKENS_LIB_NPM" #&& git
 
-echo '\033[1;31m Show git status after commands \033[0m'
+echo '\033[1;31m >>>> Show git status after commands \033[0m'
 git status
-
-

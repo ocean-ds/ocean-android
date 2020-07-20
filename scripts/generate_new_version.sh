de00673cd2e68879ef0c7cd13b2fd7772592380a
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2020-07-20 12:19:28 -0300

    DSO-33 - Fixing CI config

--- scripts/generate_new_version.sh
+++ scripts/generate_new_version.sh
@@ -18,15 +18,18 @@ npm install @useblu/tokens@beta --save-dev
 echo '\033[1;31m >>>> Copying resources XML file \033[0m'
 cp node_modules/@useblu/tokens/dist/android/tokens.xml ocean-ds-tokens/src/main/res/values/tokens.xml
 
+echo '\033[1;31m >>>> Deleting all old font files \033[0m'
+rm -rf ocean-ds-tokens/src/main/res/font/*.ttf
+
 echo '\033[1;31m >>>> Copying all font files \033[0m'
 cp node_modules/@useblu/tokens/dist/assets/fonts/*/*.ttf ocean-ds-tokens/src/main/res/font
 
 cd ocean-ds-tokens/src/main/res/font
 
-echo '\033[1;31m >>>> Renaming all font files \033[0m'
+echo '\033[1;31m >>>> Renaming all font files. \033[0m'
 for f in *.ttf
 do
-	mv $f $(echo $f |
+	mv -f $f $(echo $f |
 	sed -e 's/Avenir/font_family_highlight/g' |
 	sed -e 's/NunitoSans/font_family_base/g' |
 	sed -e 's/-/_/g' |
@@ -37,18 +40,29 @@ do
 	tr '[:upper:]' '[:lower:]')
 done
 
-cd ../../../../..
-
+echo '\033[1;31m >>>> What is the current DIR?\033[0m'
 pwd
 
-echo '\033[1;31m >>>> Show git status \033[0m'
+cd ../../../../..
+
+echo '\033[1;31m >>>> What is the current DIR?\033[0m'
+pwd
+
+echo '\033[1;31m >>>> Show git status. \033[0m'
 git status
 
 echo '\033[1;31m >>>> Running gradlew build \033[0m'
 ./gradlew clean assembleRelease publish --stacktrace --info --debug
 
-echo '\033[1;31m >>>> Add and Commit new classes \033[0m'
-git add . && git commit -am "Generating classes based on @useblu/tokens npm lib version: $LATEST_VERSION_TOKENS_LIB_NPM" #&& git
+echo '\033[1;31m >>>> Setting upstream branch: '${CIRCLE_BRANCH}'. \033[0m'
+git push --set-upstream origin ${CIRCLE_BRANCH}
 
-echo '\033[1;31m >>>> Show git status after commands \033[0m'
+echo '\033[1;31m >>>> Setting user & email. \033[0m'
+git config user.email "luis.fernandez@mobiplus.com.br"
+git config user.name "Luis Fernandez"
+
+echo '\033[1;31m >>>> Add and Commit new classes. \033[0m'
+git add . && git commit -am "Generating classes based on @useblu/tokens npm lib version: $LATEST_VERSION_TOKENS_LIB_NPM" && git push
+
+echo '\033[1;31m >>>> Show git status after commands.  \033[0m'
 git status

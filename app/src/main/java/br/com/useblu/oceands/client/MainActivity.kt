338566d7918be952ed44a411d879c7d3b43c1d16
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2020-07-31 08:55:44 -0300

    DSO-31 - Enabling DataBinding usage

--- app/src/main/java/br/com/useblu/oceands/client/MainActivity.kt
+++ app/src/main/java/br/com/useblu/oceands/client/MainActivity.kt
@@ -2,10 +2,16 @@ package br.com.useblu.oceands.client
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import br.com.useblu.oceands.client.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }

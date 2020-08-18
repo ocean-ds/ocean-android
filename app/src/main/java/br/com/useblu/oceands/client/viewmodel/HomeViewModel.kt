ea4425f78845e9b48cf6a8e659b018f8d0864a2d
mario <mario_feles@live.com>
2020-08-18 16:28:23 -0300

    implements click button text

--- app/src/main/java/br/com/useblu/oceands/client/viewmodel/HomeViewModel.kt
+++ app/src/main/java/br/com/useblu/oceands/client/viewmodel/HomeViewModel.kt
@@ -3,11 +3,17 @@ package br.com.useblu.oceands.client.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
 
     private val _showProgress = MutableLiveData<Boolean>()
     val showProgress: LiveData<Boolean> get() = _showProgress
+
+    private val _showToast = MutableLiveData<Boolean>()
+    val showToast: LiveData<Boolean> get() = _showToast
 
     fun onClickButton() {
         showProgress.let {
@@ -22,5 +28,14 @@ class HomeViewModel : ViewModel() {
                 }
             }
         ).start()
+    }
+
+    fun showToast() {
+        _showToast.postValue(true)
+
+        GlobalScope.launch {
+            delay(1000)
+            _showToast.postValue(false)
+        }
     }
 }

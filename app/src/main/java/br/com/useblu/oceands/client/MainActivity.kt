680680ae808a1535c8d99fc3078b3716137d58c6
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2020-08-25 11:40:41 -0300

    Fixing icon color when buttons changes the states. See: https://stackoverflow.com/a/36886237/659272

--- app/src/main/java/br/com/useblu/oceands/client/MainActivity.kt
+++ app/src/main/java/br/com/useblu/oceands/client/MainActivity.kt
@@ -3,15 +3,21 @@ package br.com.useblu.oceands.client
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import br.com.useblu.oceands.client.databinding.ActivityMainBinding
 import br.com.useblu.oceands.client.viewmodel.HomeViewModel
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: HomeViewModel
+
+    init {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

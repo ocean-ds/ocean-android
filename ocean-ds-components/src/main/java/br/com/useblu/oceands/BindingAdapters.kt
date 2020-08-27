613c2a9e31eac82cd7da4357e73a4e2a4fc35f40
mario <mario_feles@live.com>
2020-08-27 20:02:14 -0300

    implements binding adapter span bold

--- ocean-ds-components/src/main/java/br/com/useblu/oceands/BindingAdapters.kt
+++ ocean-ds-components/src/main/java/br/com/useblu/oceands/BindingAdapters.kt
@@ -1,6 +1,8 @@
 package br.com.useblu.oceands
 
 import android.view.View
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("app:ocean_visible_or_invisible")
@@ -8,4 +10,7 @@ fun setVisibleOrInvisible(view: View, status: Boolean) {
     view.visibility = if (status) View.VISIBLE else View.INVISIBLE
 }
 
-
+@BindingAdapter("app:ocean_text_span_bold")
+fun setTextSpanBold(view: TextView, text: String?) {
+    view.text = HtmlCompat.fromHtml("<html><body>$text</body></html>", HtmlCompat.FROM_HTML_MODE_LEGACY)
+}

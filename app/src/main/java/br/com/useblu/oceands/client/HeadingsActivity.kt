a75a983889ae00da1fee38d067785205cc9c1ce3
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2020-09-14 11:03:21 -0300

    DSO-161 - Fixing Heading not showing text problem

--- app/src/main/java/br/com/useblu/oceands/client/HeadingsActivity.kt
+++ app/src/main/java/br/com/useblu/oceands/client/HeadingsActivity.kt
@@ -1,11 +1,17 @@
 package br.com.useblu.oceands.client
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import br.com.useblu.oceands.client.databinding.ActivityHeadingsBinding
 
 class HeadingsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHeadingsBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_headings)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_headings)
+        binding.lifecycleOwner = this
     }
 }

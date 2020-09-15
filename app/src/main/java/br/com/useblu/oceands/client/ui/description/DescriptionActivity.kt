5658539d81e327e01456d9ef95be523f122aea24
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2020-09-15 09:40:49 -0300

    DSO-161 - Fixing description screen

--- app/src/main/java/br/com/useblu/oceands/client/ui/description/DescriptionActivity.kt
+++ app/src/main/java/br/com/useblu/oceands/client/ui/description/DescriptionActivity.kt
@@ -2,11 +2,17 @@ package br.com.useblu.oceands.client.ui.description
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import br.com.useblu.oceands.client.R
+import br.com.useblu.oceands.client.databinding.ActivityDescriptionBinding
+import br.com.useblu.oceands.client.databinding.ActivityParagraphBinding
 
 class DescriptionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDescriptionBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_description)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_description)
+        binding.lifecycleOwner = this
     }
 }
--- app/src/main/java/br/com/useblu/oceands/client/ui/headings/HeadingsActivity.kt
+++ app/src/main/java/br/com/useblu/oceands/client/ui/headings/HeadingsActivity.kt
@@ -9,7 +9,6 @@ import br.com.useblu.oceands.client.databinding.ActivityHeadingsBinding
 class HeadingsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHeadingsBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_headings)

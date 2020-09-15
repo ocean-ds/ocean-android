6cf6e09e8b1ffd85473616f3dd394c67955eb32d
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2020-09-15 09:28:10 -0300

    DSO-161 - Fixing paragraph screen

--- app/src/main/java/br/com/useblu/oceands/client/ui/paragraph/ParagraphActivity.kt
+++ app/src/main/java/br/com/useblu/oceands/client/ui/paragraph/ParagraphActivity.kt
@@ -2,11 +2,17 @@ package br.com.useblu.oceands.client.ui.paragraph
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import br.com.useblu.oceands.client.R
+import br.com.useblu.oceands.client.databinding.ActivityHeadingsBinding
+import br.com.useblu.oceands.client.databinding.ActivityParagraphBinding
 
 class ParagraphActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityParagraphBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_paragraph)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_paragraph)
+        binding.lifecycleOwner = this
     }
 }

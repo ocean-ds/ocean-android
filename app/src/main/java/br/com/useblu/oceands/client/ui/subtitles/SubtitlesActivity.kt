d84ac0e2627d26f354cfd5166fbd1a539bbb68f6
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2020-09-15 09:23:18 -0300

    DSO-161 - Fixing subtitle screen

--- app/src/main/java/br/com/useblu/oceands/client/ui/subtitles/SubtitlesActivity.kt
+++ app/src/main/java/br/com/useblu/oceands/client/ui/subtitles/SubtitlesActivity.kt
@@ -2,11 +2,18 @@ package br.com.useblu.oceands.client.ui.subtitles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import br.com.useblu.oceands.client.R
+import br.com.useblu.oceands.client.databinding.ActivityHeadingsBinding
+import br.com.useblu.oceands.client.databinding.ActivitySubtitlesBinding
 
 class SubtitlesActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySubtitlesBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_subtitles)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_subtitles)
+        binding.lifecycleOwner = this
     }
 }

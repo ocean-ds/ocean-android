6879cfcd469cb9a0630ae981d0fb590fe34bb711
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2020-09-15 09:41:02 -0300

    DSO-161 - Fixing caption screen

--- app/src/main/java/br/com/useblu/oceands/client/ui/caption/CaptionActivity.kt
+++ app/src/main/java/br/com/useblu/oceands/client/ui/caption/CaptionActivity.kt
@@ -2,11 +2,17 @@ package br.com.useblu.oceands.client.ui.caption
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import br.com.useblu.oceands.client.R
+import br.com.useblu.oceands.client.databinding.ActivityCaptionBinding
+import br.com.useblu.oceands.client.databinding.ActivityHeadingsBinding
 
 class CaptionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCaptionBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_caption)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_caption)
+        binding.lifecycleOwner = this
     }
 }

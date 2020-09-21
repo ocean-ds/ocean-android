ba1f7cef1ba0d573efa02f47cb27458ff49438fc
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2020-09-21 12:26:13 -0300

    DSO-27 - Creating new test component to represent the button structure

--- ocean-ds-components/src/main/java/br/com/useblu/oceands/BindingAdapters.kt
+++ ocean-ds-components/src/main/java/br/com/useblu/oceands/BindingAdapters.kt
@@ -14,6 +14,11 @@ fun setVisibleOrInvisible(view: View, status: Boolean) {
     view.visibility = if (status) View.VISIBLE else View.INVISIBLE
 }
 
+@BindingAdapter("app:ocean_visible_or_gone")
+fun setVisibleOrGone(view: View, status: Boolean) {
+    view.visibility = if (status) View.VISIBLE else View.GONE
+}
+
 @BindingAdapter("app:ocean_text_from_html")
 fun setTextFromHtml(view: TextView, text: String?) {
     view.text = HtmlCompat.fromHtml(text ?: "", HtmlCompat.FROM_HTML_MODE_LEGACY)

0c7eeea35c37b7943f6060aec4746e3091d3f561
Mario Feles <mario.feles@useblu.com.br>
2021-06-08 17:58:13 -0300

    migration components

---
+++ ocean-components/src/main/java/br/com/useblu/oceands/OceanToast.kt
@@ -0,0 +1,73 @@
+package br.com.useblu.oceands
+
+import android.app.Activity
+import android.view.Gravity
+import android.widget.Toast
+import br.com.useblu.oceands.databinding.OceanToastBinding
+
+
+class OceanToast(
+        private val activity: Activity
+) {
+
+    private lateinit var binding: OceanToastBinding
+    private var message: String? = null
+    private var type: OceanToastType? = null
+
+    fun show(): OceanToast {
+
+        binding = OceanToastBinding.inflate(activity.layoutInflater)
+
+        this.message?.let {
+            binding.message.text = it
+        }
+
+        type?.let {
+            when (type) {
+                is OceanToastType.Information -> {
+                    binding.icon.setImageResource(R.drawable.icon_information_snackbar)
+                }
+                is OceanToastType.Error -> {
+                    binding.icon.setImageResource(R.drawable.icon_error_snackbar)
+                }
+                is OceanToastType.Success -> {
+                    binding.icon.setImageResource(R.drawable.icon_sucess_snackbar)
+                }
+                is OceanToastType.Warning -> {
+                    binding.icon.setImageResource(R.drawable.icon_warning_snackbar)
+                }
+            }
+        }
+
+        with(Toast(activity.applicationContext)) {
+            duration = Toast.LENGTH_LONG
+            setGravity(Gravity.CENTER_VERTICAL, 0, 580)
+            view = binding.root
+            show()
+        }
+
+        return this
+    }
+
+    fun withMessage(message: String): OceanToast {
+        this.message = message
+        return this
+    }
+
+    fun withMessage(message: Int): OceanToast {
+        this.message = activity.getString(message)
+        return this
+    }
+
+    fun withType(type: OceanToastType): OceanToast {
+        this.type = type
+        return this
+    }
+
+    sealed class OceanToastType {
+        object Information : OceanToastType()
+        object Error : OceanToastType()
+        object Success : OceanToastType()
+        object Warning : OceanToastType()
+    }
+}

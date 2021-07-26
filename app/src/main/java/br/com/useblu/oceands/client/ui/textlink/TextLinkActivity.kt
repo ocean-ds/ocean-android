d8e7fb98e21ac9a4d409369bdfbff7ed6fdb41a5
mario <mario_feles@live.com>
2021-07-26 11:01:05 -0300

    chore: add text link inverse and button critical text

---
+++ app/src/main/java/br/com/useblu/oceands/client/ui/textlink/TextLinkActivity.kt
@@ -0,0 +1,23 @@
+package br.com.useblu.oceands.client.ui.textlink
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import br.com.useblu.oceands.client.R
+import br.com.useblu.oceands.client.databinding.ActivitySwitchsBinding
+import br.com.useblu.oceands.client.databinding.ActivityTextLinkBinding
+
+class TextLinkActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTextLinkBinding
+    private lateinit var viewModel: TextLinkViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_text_link)
+        binding.lifecycleOwner = this
+        viewModel = ViewModelProvider(this)[TextLinkViewModel::class.java]
+        binding.viewmodel = viewModel
+    }
+}

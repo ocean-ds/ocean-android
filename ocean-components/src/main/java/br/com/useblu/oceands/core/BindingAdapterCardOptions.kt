e015392cef08257a3f35e7c7dc0ed2795631ef20
mario <mario_feles@live.com>
2021-07-26 21:50:28 -0300

    feat: implements subtitle options card

---
+++ ocean-components/src/main/java/br/com/useblu/oceands/core/BindingAdapterCardOptions.kt
@@ -0,0 +1,34 @@
+package br.com.useblu.oceands.core
+
+import android.widget.RadioButton
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+import br.com.useblu.oceands.R
+
+
+@BindingAdapter("app:ocean_title", "app:ocean_subtitle", "app:ocean_disabled")
+fun setTitleAndSubtitle(
+    radioButton: RadioButton,
+    title: String?,
+    subtitle: String?,
+    disabled: Boolean
+) {
+
+    val darkUp = ContextCompat.getColor(
+        radioButton.context,
+        R.color.ocean_color_interface_dark_up
+    )
+
+    val primaryDown = ContextCompat.getColor(
+        radioButton.context,
+        R.color.ocean_color_brand_primary_down
+    )
+
+    val html = if (disabled) {
+        "<font color='$darkUp'>$title</font><br><font color='$darkUp'>$subtitle</font>"
+    } else {
+        "<font color='$primaryDown'>$title</font><br><font color='$darkUp'>$subtitle</font>"
+    }
+
+    radioButton.text = html.parseAsHtml()
+}

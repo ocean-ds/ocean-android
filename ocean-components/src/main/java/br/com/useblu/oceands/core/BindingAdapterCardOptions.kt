729ba6297fb053d8e81fd344aea9a69674a30a18
mario <mario_feles@live.com>
2021-07-27 16:04:44 -0300

    feat: implements options card sm and md

--- ocean-components/src/main/java/br/com/useblu/oceands/core/BindingAdapterCardOptions.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/core/BindingAdapterCardOptions.kt
@@ -1,14 +1,19 @@
 package br.com.useblu.oceands.core
 
-import android.widget.RadioButton
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
+import android.view.MotionEvent
+import android.view.animation.AnimationUtils
+import androidx.appcompat.widget.AppCompatRadioButton
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.databinding.BindingAdapter
 import br.com.useblu.oceands.R
 
 
 @BindingAdapter("app:ocean_title", "app:ocean_subtitle", "app:ocean_disabled")
 fun setTitleAndSubtitle(
-    radioButton: RadioButton,
+    radioButton: AppCompatRadioButton,
     title: String?,
     subtitle: String?,
     disabled: Boolean
@@ -25,10 +30,65 @@ fun setTitleAndSubtitle(
     )
 
     val html = if (disabled) {
-        "<b><font color='$darkUp'>$title</font></b><br/><font color='$darkUp'>$subtitle</font>"
+        if (subtitle.isNullOrEmpty()) {
+            "<b><font color='$darkUp'>$title</font></b>"
+        } else {
+            "<b><font color='$darkUp'>$title</font></b><br/><font color='$darkUp'>$subtitle</font>"
+        }
     } else {
-        "<b><font color='$primaryDown'>$title</b></font><br/><font color='$darkUp'>$subtitle</font>"
+        if (subtitle.isNullOrEmpty()) {
+            "<b><font color='$primaryDown'>$title</b></font>"
+        } else {
+            "<b><font color='$primaryDown'>$title</b></font><br/><font color='$darkUp'>$subtitle</font>"
+        }
     }
 
     radioButton.text = html.parseAsHtml()
+}
+
+@SuppressLint("ClickableViewAccessibility")
+@BindingAdapter("app:ocean_animation_blocked", "app:ocean_icon", "app:ocean_options_card_size")
+fun setAnimationBlocked(
+    view: AppCompatRadioButton,
+    disabled: Boolean,
+    icon: Drawable,
+    size: OptionsCardSize
+) {
+    view.isEnabled = true
+
+    if (disabled) {
+        when (size) {
+            OptionsCardSize.SM -> {
+                view.background = ContextCompat.getDrawable(
+                    view.context,
+                    R.drawable.ocean_options_card_background_sm_disabled
+                )
+            }
+            OptionsCardSize.MD -> {
+                view.background = ContextCompat.getDrawable(
+                    view.context,
+                    R.drawable.ocean_options_card_background_md_disabled
+                )
+            }
+        }
+
+
+        DrawableCompat.setTint(
+            DrawableCompat.wrap(icon),
+            ContextCompat.getColor(view.context, R.color.ocean_color_interface_dark_down)
+        )
+    }
+
+    view.setOnTouchListener { _, event ->
+        if (event.action == MotionEvent.ACTION_DOWN && disabled) {
+            val animShake = AnimationUtils.loadAnimation(view.context, R.anim.shake)
+            view.startAnimation(animShake)
+            view.isEnabled = disabled
+        }
+        false
+    }
+}
+
+enum class OptionsCardSize {
+    SM, MD
 }

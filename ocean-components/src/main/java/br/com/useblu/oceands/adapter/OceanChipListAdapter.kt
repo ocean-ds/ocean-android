696eaa0fd2bb538f707784e748f61aed4ce75393
eboliveira <32961113+eboliveira@users.noreply.github.com>
2021-12-08 10:30:06 -0300

    feat: added ocean_chips

---
+++ ocean-components/src/main/java/br/com/useblu/oceands/adapter/OceanChipListAdapter.kt
@@ -0,0 +1,163 @@
+package br.com.useblu.oceands.adapter
+
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.RecyclerView
+import br.com.useblu.oceands.R
+import br.com.useblu.oceands.core.OceanBadgeType
+import br.com.useblu.oceands.databinding.OceanChipListItemBinding
+
+class OceanChipListAdapter(
+    val items: ArrayList<OceanChipItem>,
+    val selectedItem: MutableLiveData<OceanChipItem>
+) : RecyclerView.Adapter<OceanChipListAdapter.OceanChipListViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): OceanChipListAdapter.OceanChipListViewHolder {
+        val itemBinding = OceanChipListItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        return OceanChipListViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(
+        holder: OceanChipListViewHolder,
+        position: Int
+    ) {
+        val item = items[position]
+        holder.bindView(item)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    inner class OceanChipListViewHolder(
+        private val itemBinding: OceanChipListItemBinding
+    ) : RecyclerView.ViewHolder(itemBinding.root) {
+        private val currentSelectedItem: OceanChipItem?
+            get() = items.find { it.state == OceanChipItemState.SELECTED }
+
+        @SuppressLint("NotifyDataSetChanged")
+        fun bindView(item: OceanChipItem) {
+            itemBinding.item = item
+            setItemBackground(item)
+            setItemTextColor(item)
+            setIconTint(item)
+            itemBinding.onClick = {
+                if(item != currentSelectedItem) {
+                    if (item.hasClose) {
+                        items.remove(item)
+                        notifyItemRemoved(items.indexOf(item))
+                    } else {
+                        unselectCurrent()
+                        selectItem(item)
+                        selectedItem.postValue(item)
+                    }
+                    notifyDataSetChanged()
+                }
+
+            }
+        }
+
+        private fun setItemBackground(item: OceanChipItem) {
+            val context = itemBinding.root.context
+            itemBinding.chipListItemBackground.background = when (item.state) {
+                OceanChipItemState.ERROR -> ContextCompat.getDrawable(
+                    context,
+                    R.drawable.ocean_chip_error
+                )
+                OceanChipItemState.SELECTED -> ContextCompat.getDrawable(
+                    context,
+                    R.drawable.ocean_chip_selected
+                )
+                else -> ContextCompat.getDrawable(
+                    context,
+                    R.drawable.ocean_chip_default
+                )
+            }
+        }
+
+        private fun setItemTextColor(item: OceanChipItem) {
+            val context = itemBinding.root.context
+            val textColor = when (item.state) {
+                OceanChipItemState.ERROR -> ContextCompat.getColor(
+                    context,
+                    R.color.ocean_color_brand_primary_down
+                )
+                OceanChipItemState.SELECTED -> ContextCompat.getColor(
+                    context,
+                    R.color.ocean_color_interface_light_pure
+                )
+                OceanChipItemState.DISABLED -> ContextCompat.getColor(
+                    context,
+                    R.color.ocean_color_interface_dark_up
+                )
+                OceanChipItemState.DEFAULT -> ContextCompat.getColor(
+                    context,
+                    R.color.ocean_color_brand_primary_down
+                )
+            }
+
+            itemBinding.label.setTextColor(textColor)
+        }
+
+        private fun setIconTint(item: OceanChipItem) {
+            val context = itemBinding.root.context
+            val iconColor = when (item.state) {
+                OceanChipItemState.ERROR -> ContextCompat.getColor(
+                    context,
+                    R.color.ocean_color_brand_primary_down
+                )
+                OceanChipItemState.SELECTED -> ContextCompat.getColor(
+                    context,
+                    R.color.ocean_color_interface_light_pure
+                )
+                OceanChipItemState.DISABLED -> ContextCompat.getColor(
+                    context,
+                    R.color.ocean_color_interface_dark_up
+                )
+                OceanChipItemState.DEFAULT -> ContextCompat.getColor(
+                    context,
+                    R.color.ocean_color_brand_primary_down
+                )
+            }
+
+            itemBinding.leftIcon.setColorFilter(iconColor)
+        }
+
+        private fun selectItem(item: OceanChipItem) {
+            val selectedItem = items.find { it.id == item.id }
+            selectedItem?.state = OceanChipItemState.SELECTED
+        }
+
+        private fun unselectCurrent() {
+            currentSelectedItem?.state = OceanChipItemState.DEFAULT
+        }
+    }
+}
+
+data class OceanChipItem(
+    val label: String,
+    val id: Int,
+    val badge: Badge? = null,
+    val icon: Drawable? = null,
+    val hasClose: Boolean = false,
+    var state: OceanChipItemState = OceanChipItemState.DEFAULT
+)
+
+data class Badge(
+    val text: Int,
+    val type: OceanBadgeType
+)
+
+enum class OceanChipItemState {
+    ERROR, DISABLED, SELECTED, DEFAULT
+}
---
+++ ocean-components/src/main/java/br/com/useblu/oceands/core/BindingAdapterChip.kt
@@ -0,0 +1,52 @@
+package br.com.useblu.oceands.core
+
+import android.content.Context
+import android.graphics.Rect
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import br.com.useblu.oceands.adapter.OceanChipItem
+import br.com.useblu.oceands.adapter.OceanChipListAdapter
+
+
+@BindingAdapter("chips", "selectedItem", "itemsSpacing")
+fun setChipsAdapter(
+    recyclerView: RecyclerView,
+    chips: ArrayList<OceanChipItem>,
+    selectedItem: MutableLiveData<OceanChipItem>,
+    itemsSpacing: Float
+) {
+    if (recyclerView.adapter == null) {
+        val adapter = OceanChipListAdapter(chips, selectedItem)
+        val layoutManager =
+            LinearLayoutManager(recyclerView.context, LinearLayoutManager.HORIZONTAL, false)
+        val divider = ItemsDivider(recyclerView.context, layoutManager.orientation, itemsSpacing)
+        val drawable = ContextCompat.getDrawable(recyclerView.context, android.R.color.transparent)
+        if (drawable != null) divider.setDrawable(drawable)
+
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = layoutManager
+        recyclerView.addItemDecoration(divider)
+    }
+}
+
+class ItemsDivider(private val context: Context, orientation: Int, private val itemsSpacing: Float) :
+    DividerItemDecoration(context, orientation) {
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        val position = parent.getChildAdapterPosition(view)
+        if (position == state.itemCount - 1)
+            outRect.setEmpty()
+        else
+            outRect.set(0, 0, itemsSpacing.toInt(), 0)
+    }
+}

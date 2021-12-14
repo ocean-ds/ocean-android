e89f87d17d021bbd2eda6aceed9a126b028a492a
eboliveira <32961113+eboliveira@users.noreply.github.com>
2021-12-14 13:54:10 -0300

    feat: added checkbox on transaction list item

--- app/src/main/java/br/com/useblu/oceands/client/ui/transactionlistitem/TransactionListItemViewModel.kt
+++ app/src/main/java/br/com/useblu/oceands/client/ui/transactionlistitem/TransactionListItemViewModel.kt
@@ -1,11 +1,25 @@
 package br.com.useblu.oceands.client.ui.transactionlistitem
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 
-class TransactionListItemViewModel: ViewModel() {
+class TransactionListItemViewModel(application: Application): AndroidViewModel(application) {
+    private val _hasCheckbox = MutableLiveData(false)
+    val hasCheckbox: MutableLiveData<Boolean> get() = _hasCheckbox
 
     fun loadData() {
 
     }
 
+    fun showCheckbox(view: View): Boolean {
+        _hasCheckbox.postValue(true)
+        return true
+    }
+
+    fun checkboxClick(checked: Boolean) {
+        Toast.makeText(getApplication(), "Checked: $checked", Toast.LENGTH_SHORT).show()
+    }
 }

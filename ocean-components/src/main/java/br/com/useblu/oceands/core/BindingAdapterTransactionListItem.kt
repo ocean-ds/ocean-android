65bb72853cb63a6efd202f257bc7ce4a2be772e3
eboliveira <32961113+eboliveira@users.noreply.github.com>
2021-12-17 13:32:39 -0300

    fix(transaction_item): click directly on checkbox changes behaviour

--- ocean-components/src/main/java/br/com/useblu/oceands/core/BindingAdapterTransactionListItem.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/core/BindingAdapterTransactionListItem.kt
@@ -1,12 +1,8 @@
 package br.com.useblu.oceands.core
 
 import android.transition.ChangeBounds
-import android.transition.Transition
 import android.transition.TransitionManager
 import android.view.View
-import android.view.animation.Animation
-import android.view.animation.Transformation
-import android.view.animation.TranslateAnimation
 import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
@@ -16,7 +12,6 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import br.com.useblu.oceands.R
 import br.com.useblu.oceands.databinding.OceanCheckboxBinding
-import android.view.animation.AnticipateOvershootInterpolator
 
 
 @BindingAdapter("click", "hasCheckbox", "index", "selectionMode")
@@ -29,31 +24,57 @@ fun setClickListener(
 ) {
     val checkbox = layout.findViewById<View>(R.id.checkbox)
     if (hasCheckbox == true) {
-        checkbox.setOnClickListener { }
         selectionMode?.observe(layout.context as LifecycleOwner) {
-            val alreadyOnSelectionMode = alreadyOnSelectionMode(checkbox)
-            if (it.first) {
-                if (alreadyOnSelectionMode) return@observe
-
-                showCheckboxWithAnimation(layout, checkbox, checkboxChecked = it.second == index)
-            } else {
-                if (alreadyOnSelectionMode.not()) return@observe
-
-                hideCheckboxWithAnimation(layout, checkbox)
-            }
+            enterOrLeaveSelectionMode(checkbox, it, layout, index)
         }
     }
-    layout.setOnClickListener {
-        if (hasCheckbox == true) {
-            if (!alreadyOnSelectionMode(checkbox)) {
-                selectionMode?.postValue(true to (index ?: -1))
-            }
-        }
+    setOnClickListeners(layout, checkbox, hasCheckbox, selectionMode, index, click)
+}
 
-        click?.invoke(index ?: -1)
+private fun setOnClickListeners(
+    layout: LinearLayoutCompat,
+    checkbox: View,
+    hasCheckbox: Boolean?,
+    selectionMode: MutableLiveData<Pair<Boolean, Int>>?,
+    index: Int?,
+    click: ((Int) -> Unit)?
+) {
+    layout.setOnClickListener {
         oceanCheckboxBinding(checkbox)?.let {
-            it.checked = it.checked?.not() ?: false
+            it.checked = it.checked?.not() ?: true
         }
+    }
+    oceanCheckboxBinding(checkbox)?.let {
+        it.click = {
+            oceanCheckboxBinding(checkbox)?.let { binding ->
+                binding.checked = it
+            }
+            if (hasCheckbox == true) {
+                if (!alreadyOnSelectionMode(checkbox)) {
+                    selectionMode?.postValue(true to (index ?: -1))
+                }
+            }
+
+            click?.invoke(index ?: -1)
+        }
+    }
+}
+
+private fun enterOrLeaveSelectionMode(
+    checkbox: View,
+    it: Pair<Boolean, Int>,
+    layout: LinearLayoutCompat,
+    index: Int?
+) {
+    val alreadyOnSelectionMode = alreadyOnSelectionMode(checkbox)
+    if (it.first) {
+        if (alreadyOnSelectionMode) return
+
+        showCheckboxWithAnimation(layout, checkbox, checkboxChecked = it.second == index)
+    } else {
+        if (alreadyOnSelectionMode.not()) return
+
+        hideCheckboxWithAnimation(layout, checkbox)
     }
 }
 

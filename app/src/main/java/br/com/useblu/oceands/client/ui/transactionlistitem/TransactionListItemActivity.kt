02f86e5a7ba9d54c079efa5d590f780c81e0cb36
eboliveira <32961113+eboliveira@users.noreply.github.com>
2021-12-20 16:55:28 -0300

    feat(transaction_list_item): added a recyclerview on app module for testing

--- app/src/main/java/br/com/useblu/oceands/client/ui/transactionlistitem/TransactionListItemActivity.kt
+++ app/src/main/java/br/com/useblu/oceands/client/ui/transactionlistitem/TransactionListItemActivity.kt
@@ -1,11 +1,14 @@
 package br.com.useblu.oceands.client.ui.transactionlistitem
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.useblu.oceands.client.R
 import br.com.useblu.oceands.client.databinding.ActivityTransactionListItemBinding
+import br.com.useblu.oceands.core.OceanTransactionListUIModel
 
 class TransactionListItemActivity : AppCompatActivity() {
 
@@ -25,6 +28,11 @@ class TransactionListItemActivity : AppCompatActivity() {
         initObservers()
     }
 
+    override fun onResume() {
+        super.onResume()
+        initRecyclerView()
+    }
+
     private fun initObservers() {
         viewModel.clickedItem.observe(this) { index ->
             with(selectedItems) {
@@ -35,4 +43,57 @@ class TransactionListItemActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("NotifyDataSetChanged")
+    private fun initRecyclerView() {
+        binding.transactionListRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = TransactionListItemAdapter(selectionMode = viewModel.selectionMode) {
+                viewModel.click(it)
+            }.apply {
+                val list = listOf(
+                    OceanTransactionListUIModel(
+                        tagTitle = "Tag 1",
+                        value = "-1000000.00",
+                        primaryLabel = "A LABEL WHICH IS VERY LARGE"
+                    ),
+                    OceanTransactionListUIModel(
+                        tagTitle = "Tag 2",
+                        value = "-1000000.00",
+                        primaryLabel = "A LABEL WHICH IS VERY LARGE"
+                    ),
+                    OceanTransactionListUIModel(
+                        tagTitle = "Tag 3",
+                        value = "-1000000.00",
+                        primaryLabel = "A LABEL WHICH IS VERY LARGE"
+                    ),
+                    OceanTransactionListUIModel(
+                        tagTitle = "Tag 4",
+                        value = "-1000000.00",
+                        primaryLabel = "A LABEL WHICH IS VERY LARGE"
+                    ),
+                    OceanTransactionListUIModel(
+                        tagTitle = "Tag 5",
+                        value = "-1000000.00",
+                        primaryLabel = "A LABEL WHICH IS VERY LARGE"
+                    ),
+                    OceanTransactionListUIModel(
+                        tagTitle = "Tag 6",
+                        value = "-1000000.00",
+                        primaryLabel = "A LABEL WHICH IS VERY LARGE"
+                    ),
+                    OceanTransactionListUIModel(
+                        tagTitle = "Tag 7",
+                        value = "-1000000.00",
+                        primaryLabel = "A LABEL WHICH IS VERY LARGE"
+                    ),
+                    OceanTransactionListUIModel(
+                        tagTitle = "Tag 8",
+                        value = "-1000000.00",
+                        primaryLabel = "A LABEL WHICH IS VERY LARGE"
+                    )
+                )
+                submitList(list)
+            }
+        }
+    }
 }
---
+++ app/src/main/java/br/com/useblu/oceands/client/ui/transactionlistitem/TransactionListItemAdapter.kt
@@ -0,0 +1,60 @@
+package br.com.useblu.oceands.client.ui.transactionlistitem
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import br.com.useblu.oceands.core.OceanTransactionListUIModel
+import br.com.useblu.oceands.databinding.OceanTransactionListItemBinding
+
+class TransactionListItemAdapter(
+    private val selectionMode: MutableLiveData<Boolean>,
+    private val onItemClicked: (Int) -> Unit
+) : ListAdapter<OceanTransactionListUIModel, TransactionListItemAdapter.TransactionListItemViewHolder>(
+    Companion
+) {
+    companion object : DiffUtil.ItemCallback<OceanTransactionListUIModel>() {
+        override fun areItemsTheSame(
+            oldItem: OceanTransactionListUIModel,
+            newItem: OceanTransactionListUIModel
+        ): Boolean =
+            oldItem === newItem
+
+        override fun areContentsTheSame(
+            oldItem: OceanTransactionListUIModel,
+            newItem: OceanTransactionListUIModel
+        ): Boolean =
+            oldItem == newItem
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TransactionListItemViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = OceanTransactionListItemBinding.inflate(layoutInflater, parent, false)
+        return TransactionListItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TransactionListItemViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item, position)
+    }
+
+    inner class TransactionListItemViewHolder(
+        private val binding: OceanTransactionListItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: OceanTransactionListUIModel, position: Int) {
+            binding.tagTitle = item.tagTitle
+            binding.primaryLabel = item.primaryLabel
+            binding.value = item.value
+            binding.hasCheckbox = true
+            binding.selectionMode = selectionMode
+            binding.click = onItemClicked
+            binding.index = position
+            binding.executePendingBindings()
+        }
+    }
+}

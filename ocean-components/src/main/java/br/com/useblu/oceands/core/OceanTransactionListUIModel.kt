4ef972523ec57d2a51b865dcb68a59144e576778
eboliveira <32961113+eboliveira@users.noreply.github.com>
2021-12-28 16:21:23 -0300

    feat(transaction_list_item): make data class parcelable

--- ocean-components/src/main/java/br/com/useblu/oceands/core/OceanTransactionListUIModel.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/core/OceanTransactionListUIModel.kt
@@ -1,7 +1,10 @@
 package br.com.useblu.oceands.core
 
 import android.graphics.drawable.Drawable
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class OceanTransactionListUIModel(
     val icon: Drawable? = null,
     val highlightedLabel: String? = null,
@@ -13,4 +16,4 @@ data class OceanTransactionListUIModel(
     val valueWithSignal: Boolean = true,
     val tagTitle: String? = null,
     val time: String? = null
-)
+): Parcelable

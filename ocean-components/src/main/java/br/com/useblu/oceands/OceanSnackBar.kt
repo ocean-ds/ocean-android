579cbd73962309faf72dda493f44698d59881aa1
Vinicius Consulmagnos Romeiro <vinicius.romeiro@gmail.com>
2021-12-29 17:28:53 -0300

    feat: add action in snackbar (#181)
    * feat: add action in snackbar
    
    * fix: move set action color

--- ocean-components/src/main/java/br/com/useblu/oceands/OceanSnackBar.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/OceanSnackBar.kt
@@ -10,11 +10,12 @@ import com.google.android.material.snackbar.Snackbar
 class OceanSnackBar(
     private val container: View,
     private val message: String,
-    private val type: OceanSnackBarType
+    private val type: OceanSnackBarType,
+    private val actionText: String? = null,
+    private val actionClick: View.OnClickListener? = null
 ) {
     fun show() {
         Snackbar.make(container, message, Snackbar.LENGTH_LONG).apply {
-
             this.duration = 5000
             this.view.background =
                 ContextCompat.getDrawable(context, (R.drawable.ocean_snackbar_background))
@@ -58,6 +59,28 @@ class OceanSnackBar(
                 this.setTextColor(getColor(context, R.color.ocean_color_interface_light_pure))
                 this.typeface = ResourcesCompat.getFont(context, R.font.font_family_base_regular)
                 this.setPadding(0,0,0,0)
+            }
+
+            actionText?.let {
+                setAction(it, actionClick)
+                this.view.findViewById<TextView>(com.google.android.material.R.id.snackbar_action)
+                    .apply {
+                        isAllCaps = false
+                    }
+                when (type) {
+                    is OceanSnackBarType.Information -> {
+                        setActionTextColor(getColor(context, R.color.ocean_color_brand_primary_pure))
+                    }
+                    is OceanSnackBarType.Error -> {
+                        setActionTextColor(getColor(context, R.color.ocean_color_status_negative_pure))
+                    }
+                    is OceanSnackBarType.Success -> {
+                        setActionTextColor(getColor(context, R.color.ocean_color_status_positive_pure))
+                    }
+                    is OceanSnackBarType.Warning -> {
+                        setActionTextColor(getColor(context, R.color.ocean_color_status_neutral_pure))
+                    }
+                }
             }
 
             show()

d3ac2a47c646c472bd1c466405f1a27c0deb3b7e
thomas <thomasmarques@icloud.com>
2022-01-10 11:54:44 -0300

    feat: add long click pressed in view

--- app/src/main/java/br/com/useblu/oceands/client/ui/listitem/ListItemViewModel.kt
+++ app/src/main/java/br/com/useblu/oceands/client/ui/listitem/ListItemViewModel.kt
@@ -8,7 +8,14 @@ class ListItemViewModel : ViewModel() {
     private val _clickedLink: MutableLiveData<String> = MutableLiveData()
     val clickedLink: LiveData<String> get() = _clickedLink
 
+    private val _longClickPressed: MutableLiveData<Boolean> = MutableLiveData()
+    val longClickPressed: LiveData<Boolean> get() = _longClickPressed
+
     fun clickedLink(id: String) {
         _clickedLink.postValue(id)
+    }
+
+    fun longClick() {
+        _longClickPressed.postValue(true)
     }
 }

f785deb95513a42debde30530e11271ef7cb7d9f
Mario Feles dos Santos Junior <mario_feles@live.com>
2022-03-28 17:13:21 -0300

    feat: refactor binding adapter (#222)

---
+++ ocean-components/src/main/java/br/com/useblu/oceands/core/BindingAdapterSwitch.kt
@@ -0,0 +1,15 @@
+package br.com.useblu.oceands.core
+
+import androidx.appcompat.widget.SwitchCompat
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("app:isChecked", "app:setOnChecked")
+fun setOnChecked(
+    switch: SwitchCompat,
+    isChecked: Boolean,
+    setOnChecked: (Boolean) -> Unit
+) {
+    switch.setOnCheckedChangeListener(null)
+    switch.isChecked = isChecked
+    switch.setOnCheckedChangeListener { _, selected -> setOnChecked.invoke(selected) }
+}

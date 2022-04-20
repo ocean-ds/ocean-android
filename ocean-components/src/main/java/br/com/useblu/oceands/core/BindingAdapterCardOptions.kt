bb5e019be274dcd665de53df32eadaf820a93d58
Mario Feles dos Santos Junior <mario_feles@live.com>
2022-04-20 17:53:58 -0300

    style: fix color selected ocean card option (#231)

--- ocean-components/src/main/java/br/com/useblu/oceands/core/BindingAdapterCardOptions.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/core/BindingAdapterCardOptions.kt
@@ -5,47 +5,10 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.LinearLayout
-import androidx.appcompat.widget.AppCompatRadioButton
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
 import androidx.databinding.BindingAdapter
 import br.com.useblu.oceands.R
-
-
-@BindingAdapter("app:ocean_title", "app:ocean_subtitle", "app:ocean_disabled")
-fun setTitleAndSubtitle(
-    radioButton: AppCompatRadioButton,
-    title: String?,
-    subtitle: String?,
-    disabled: Boolean
-) {
-
-    val darkDown = ContextCompat.getColor(
-        radioButton.context,
-        R.color.ocean_color_interface_dark_down
-    )
-
-    val primaryDown = ContextCompat.getColor(
-        radioButton.context,
-        R.color.ocean_color_brand_primary_down
-    )
-
-    val html = if (disabled) {
-        if (subtitle.isNullOrEmpty()) {
-            "<b><font color='$darkDown'>$title</font></b>"
-        } else {
-            "<b><font color='$darkDown'>$title</font></b><br/><font color='$darkDown'><small>$subtitle</small></font>"
-        }
-    } else {
-        if (subtitle.isNullOrEmpty()) {
-            "<b><font color='$primaryDown'>$title</font></b>"
-        } else {
-            "<b><font color='$primaryDown'>$title</font></b><br/><font color='$darkDown'><small>$subtitle</small></font>"
-        }
-    }
-
-    radioButton.text = html.parseAsHtml()
-}
 
 @SuppressLint("ClickableViewAccessibility")
 @BindingAdapter("app:ocean_animation_blocked")

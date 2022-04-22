65473471507404a078de054e9db6c30fe36acd4b
Aline Almeida Queiroz <101147453+alineaqueiroz@users.noreply.github.com>
2022-04-22 11:03:52 -0300

    feat: add component parent child text list (#227)
    * feat(xml):adiciona componente ocean_parent_child_text_list e item_parent_text_list
    feat:adiciona classes para viewModel, activity e item com chamadas na home e manifest
    
    * feat(xml):renomeia alguns ids e nomes de variáveis para equalização com iOS.
    
    * feat(xml):renomeia alguns ids e nomes de variáveis e inclusão de ícone.
    feat:Implementação dataBinding (adapter e viewModel)
    
    * feat(xml):inclusão de lib para swipe e criação do layout dos botões editar e excluir
    feat:Implementação chamada dos botões no dataBinding (adapter)
    
    * feat(xml):alteração do ícone seta no layout do ocean_parent_child_text_list e exclusão do ícone que estava anteriormente.
    
    * feat(xml):click action on component recyclerView items.
    
    * feat():click action on edit and delete buttons.
    
    * feat:add ripple to buttons
    feat:add longPress
    
    * feat: refactors log messages to toast.
    
    * feat: refactors log messages to toast
    
    Co-authored-by: Mario Feles dos Santos Junior <mario_feles@live.com>

---
+++ app/src/main/java/br/com/useblu/oceands/client/ui/parentchildtextlist/ParentChildTextListViewModel.kt
@@ -0,0 +1,46 @@
+package br.com.useblu.oceands.client.ui.parentchildtextlist
+
+import android.graphics.drawable.Drawable
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import br.com.useblu.oceands.core.OceanChildTextItem
+import br.com.useblu.oceands.core.OceanParentTextItem
+
+class ParentChildTextListViewModel : ViewModel() {
+
+    val parent = MutableLiveData<OceanParentTextItem>()
+
+    private val _showMessage: MutableLiveData<String> = MutableLiveData()
+    val showMessage: LiveData<String> get() = _showMessage
+
+    fun loadData(drawable: Drawable?) {
+        parent.postValue(
+            OceanParentTextItem(
+                image = drawable, title = "header", subTitle = "subtitle",
+                children = listOf(
+                    OceanChildTextItem(image = drawable, title = "nome", subTitle = "teste"),
+                    OceanChildTextItem(image = drawable, title = "nome", subTitle = "teste"),
+                    OceanChildTextItem(image = drawable, title = "nome", subTitle = "teste")
+                )
+            )
+        )
+    }
+
+    fun click(position: Int) {
+        _showMessage.postValue("Clicou no item: $position")
+    }
+
+    fun clickEditButton(position: Int) {
+        _showMessage.postValue("Clicou no botão Editar no item: $position")
+    }
+
+    fun clickDeleteButton(position: Int) {
+        _showMessage.postValue("Clicou no botão Excluir no item: $position")
+    }
+
+    fun longClick(position: Int) {
+        _showMessage.postValue("Clicou long press no item: $position")
+    }
+}
---
+++ ocean-components/src/main/java/br/com/useblu/oceands/core/BindAdapterParentChildItem.kt
@@ -0,0 +1,124 @@
+package br.com.useblu.oceands.core
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import br.com.useblu.oceands.databinding.ItemParentTextListBinding
+import io.sulek.ssml.OnSwipeListener
+import io.sulek.ssml.SSMLLinearLayoutManager
+
+@BindingAdapter(
+    "app:setChildren",
+    "app:clickItem",
+    "app:clickEdit",
+    "app:clickDelete",
+    "app:longClickItem"
+)
+fun setInflateChildren(
+    recyclerView: RecyclerView, children: List<OceanChildTextItem>?,
+    clickItem: ((Int) -> Unit)?,
+    clickEdit: ((Int) -> Unit)?,
+    clickDelete: ((Int) -> Unit)?,
+    longClickItem: ((Int) -> Unit)?
+) {
+
+    children?.let {
+        recyclerView.adapter = ChildrenAdapter(
+            it, onClicked = clickItem,
+            onClickedButtonEdit = clickEdit,
+            onClickedButtonDelete = clickDelete,
+            onLongClickPressed = longClickItem
+        )
+        recyclerView.layoutManager = SSMLLinearLayoutManager(recyclerView.context)
+    }
+}
+
+class ChildrenAdapter(
+    val list: List<OceanChildTextItem>,
+    val onClicked: ((Int) -> Unit)?,
+    val onClickedButtonEdit: ((Int) -> Unit)?,
+    val onClickedButtonDelete: ((Int) -> Unit)?,
+    val onLongClickPressed: ((Int) -> Unit)?
+) : RecyclerView.Adapter<ChildrenAdapter.ChildrenViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChildrenViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemParentTextListBinding.inflate(layoutInflater, parent, false)
+        return ChildrenViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ChildrenViewHolder, position: Int) {
+        val item = list[position]
+        holder.bind(
+            item,
+            position,
+            onItemClicked = {
+                onClicked?.invoke(it)
+            },
+            onItemButtonEditClicked = {
+                onClickedButtonEdit?.invoke(it)
+            },
+            onItemButtonDeleteClicked = {
+                onClickedButtonDelete?.invoke(it)
+            },
+            onItemLongClicked = {
+                onLongClickPressed?.invoke(it)
+            }
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class ChildrenViewHolder(
+        val binding: ItemParentTextListBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(
+            oceanChildTextItem: OceanChildTextItem, position: Int,
+            onItemClicked: (Int) -> Unit,
+            onItemButtonEditClicked: (Int) -> Unit,
+            onItemButtonDeleteClicked: (Int) -> Unit,
+            onItemLongClicked: (Int) -> Unit
+        ) {
+
+            binding.item = oceanChildTextItem
+            binding.click = {
+                onItemClicked.invoke(position)
+            }
+            binding.clickEditButton = {
+                onItemButtonEditClicked.invoke(position)
+            }
+            binding.clickDeleteButton = {
+                onItemButtonDeleteClicked.invoke(position)
+            }
+            binding.titleItemChild.setOnLongClickListener {
+                onItemLongClicked.invoke(position)
+                false
+            }
+            binding.subtitleItemChild.setOnLongClickListener {
+                onItemLongClicked.invoke(position)
+                false
+            }
+            binding.imageChild.setOnLongClickListener {
+                onItemLongClicked.invoke(position)
+                false
+            }
+
+            binding.swipeContainer.setOnSwipeListener(object : OnSwipeListener {
+                override fun onSwipe(isExpanded: Boolean) {
+                    oceanChildTextItem.isExpanded = isExpanded
+                }
+            })
+
+            binding.swipeContainer.apply(oceanChildTextItem.isExpanded)
+
+            binding.executePendingBindings()
+        }
+    }
+
+}

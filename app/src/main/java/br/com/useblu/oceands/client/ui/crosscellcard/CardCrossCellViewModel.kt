bcc19d8d09705567989e71aa5144e27a1a457ae8
Mario Feles dos Santos Junior <mario_feles@live.com>
2022-06-10 09:14:47 -0300

    feat: implements card cross cell (#246)

---
+++ app/src/main/java/br/com/useblu/oceands/client/ui/crosscellcard/CardCrossCellViewModel.kt
@@ -0,0 +1,12 @@
+package br.com.useblu.oceands.client.ui.crosscellcard
+
+import androidx.lifecycle.ViewModel
+
+class CardCrossCellViewModel : ViewModel() {
+
+    fun click() {
+        println("Click action")
+    }
+
+
+}

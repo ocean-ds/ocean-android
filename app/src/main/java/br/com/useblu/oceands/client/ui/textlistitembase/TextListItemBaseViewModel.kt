4a574b8406f9cd24581c5dcb069aedee5dab963f
Tarik (Julio Martins) <38379044+nordael@users.noreply.github.com>
2023-02-17 17:32:58 -0300

    feat: creating text list item base activity

---
+++ app/src/main/java/br/com/useblu/oceands/client/ui/textlistitembase/TextListItemBaseViewModel.kt
@@ -0,0 +1,14 @@
+package br.com.useblu.oceands.client.ui.textlistitembase
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TextListItemBaseViewModel : ViewModel() {
+    private val _touchItem: MutableLiveData<String> = MutableLiveData()
+    val touchItem: LiveData<String> get() = _touchItem
+
+    fun onTouchItem() {
+        _touchItem.postValue("Item touched")
+    }
+}

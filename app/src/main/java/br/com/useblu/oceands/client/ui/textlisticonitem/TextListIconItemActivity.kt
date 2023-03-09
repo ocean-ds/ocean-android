c9bac82ea4d29e72608d42dd7c3bc96a8a69fc49
Tarik (Julio Martins) <38379044+nordael@users.noreply.github.com>
2023-03-09 18:04:17 -0300

    feat: creating component text list icon item

---
+++ app/src/main/java/br/com/useblu/oceands/client/ui/textlisticonitem/TextListIconItemActivity.kt
@@ -0,0 +1,26 @@
+package br.com.useblu.oceands.client.ui.textlisticonitem
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import br.com.useblu.oceands.client.R
+import br.com.useblu.oceands.client.databinding.ActivityTextListIconItemBinding
+import br.com.useblu.oceands.client.databinding.ActivityTextListItemBaseBinding
+import br.com.useblu.oceands.client.ui.textlistitembase.TextListItemBaseViewModel
+
+class TextListIconItemActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTextListIconItemBinding
+    private lateinit var viewModel: TextListIconItemViewModel
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_text_list_icon_item)
+        binding.lifecycleOwner = this
+
+        viewModel = ViewModelProvider(this)[TextListIconItemViewModel::class.java]
+        binding.viewmodel = viewModel
+    }
+}
---
+++ app/src/main/java/br/com/useblu/oceands/client/ui/textlisticonitem/TextListIconItemViewModel.kt
@@ -0,0 +1,6 @@
+package br.com.useblu.oceands.client.ui.textlisticonitem
+
+import androidx.lifecycle.ViewModel
+
+class TextListIconItemViewModel : ViewModel() {
+}

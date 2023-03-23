72e45b4d2364bcc98419db90e8de17c16cc3c76a
Mario Feles dos Santos Junior <mario_feles@live.com>
2023-03-23 10:21:10 -0300

    feat: add click icon ocean text list icon item

--- app/src/main/java/br/com/useblu/oceands/client/ui/textlisticonitem/TextListIconItemViewModel.kt
+++ app/src/main/java/br/com/useblu/oceands/client/ui/textlisticonitem/TextListIconItemViewModel.kt
@@ -3,4 +3,8 @@ package br.com.useblu.oceands.client.ui.textlisticonitem
 import androidx.lifecycle.ViewModel
 
 class TextListIconItemViewModel : ViewModel() {
+
+    fun click(){
+        println("Click Icon")
+    }
 }

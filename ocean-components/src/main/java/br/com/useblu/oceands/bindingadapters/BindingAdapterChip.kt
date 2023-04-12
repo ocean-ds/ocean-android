980380344d3a5097b9df22dd8e9a91f251a83d97
Fernando Zanutto <fernando.zanutto@useblu.com.br>
2023-04-12 12:30:19 -0300

    feat: unused import

--- ocean-components/src/main/java/br/com/useblu/oceands/bindingadapters/BindingAdapterChip.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/bindingadapters/BindingAdapterChip.kt
@@ -3,9 +3,7 @@ package br.com.useblu.oceands.bindingadapters
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import br.com.useblu.oceands.adapter.OceanChipListAdapter
-import br.com.useblu.oceands.model.OceanBasicChip
 import br.com.useblu.oceands.model.OceanChip
-import br.com.useblu.oceands.model.OceanFilterChip
 
 @BindingAdapter("chips")
 fun setChipsAdapter(

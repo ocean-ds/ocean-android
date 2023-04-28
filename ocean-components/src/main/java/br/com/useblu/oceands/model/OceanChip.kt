ce7e64efca9349a226fab5b1caf182e60e532919
Tarik (Julio Martins) <38379044+nordael@users.noreply.github.com>
2023-04-28 14:53:48 -0300

    feat: adding a divider to basic chip

--- ocean-components/src/main/java/br/com/useblu/oceands/model/OceanChip.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/model/OceanChip.kt
@@ -13,7 +13,8 @@ data class OceanBasicChip(
     override val badge: Badge? = null,
     val icon: String? = null,
     override var state: OceanChipItemState = OceanChipItemState.INACTIVE_HOVER,
-    var onClick: (selected: Boolean) -> Unit
+    var onClick: (selected: Boolean) -> Unit,
+    val hasFilterAll: Boolean = false,
 ): OceanChip()
 
 data class OceanFilterChip(

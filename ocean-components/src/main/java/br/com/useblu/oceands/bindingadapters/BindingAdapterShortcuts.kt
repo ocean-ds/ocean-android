44b368941b829a8015b49e86d0ed58b295f26d40
MarioFelesDosSantosJunior <mario_feles@live.com>
2023-06-13 11:40:57 -0300

    feat: refactor ocean shortcuts

--- ocean-components/src/main/java/br/com/useblu/oceands/bindingadapters/BindingAdapterShortcuts.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/bindingadapters/BindingAdapterShortcuts.kt
@@ -1,29 +1,24 @@
 package br.com.useblu.oceands.bindingadapters
 
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import br.com.useblu.oceands.model.OceanShortcutItem
 import br.com.useblu.oceands.adapter.OceanShortcutsListAdapter
+import br.com.useblu.oceands.model.OceanShortcutItem
 
-@BindingAdapter("entries", "shortcutLayoutManager")
+@BindingAdapter("entries", "columns")
 fun setShortcutsAdapter(
     recyclerView: RecyclerView,
     entries: List<OceanShortcutItem>?,
-    layoutManager: LinearLayoutManager?
+    columns: Int?
 ) {
     entries?.let { list ->
         val oceanShortcutsListAdapter = OceanShortcutsListAdapter(items = list)
         recyclerView.adapter = oceanShortcutsListAdapter
 
-        if (layoutManager == null) {
-            recyclerView.layoutManager = LinearLayoutManager(
-                recyclerView.context,
-                LinearLayoutManager.HORIZONTAL,
-                false
-            )
-        } else {
-            recyclerView.layoutManager = layoutManager
-        }
+        recyclerView.layoutManager = GridLayoutManager(
+            recyclerView.context, columns ?: 2
+        )
+
     }
 }

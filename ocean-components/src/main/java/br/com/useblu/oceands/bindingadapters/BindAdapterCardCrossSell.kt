36df4aee046dbc887241bff41712fa558bc36aff
Mario Feles dos Santos Junior <mario_feles@live.com>
2023-06-28 15:13:35 -0300

    fix: adjust ocean card cross cell

--- ocean-components/src/main/java/br/com/useblu/oceands/bindingadapters/BindAdapterCardCrossSell.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/bindingadapters/BindAdapterCardCrossSell.kt
@@ -1,20 +1,45 @@
 package br.com.useblu.oceands.bindingadapters
 
+import android.graphics.drawable.GradientDrawable
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import br.com.useblu.oceands.R
 import br.com.useblu.oceands.utils.toOceanColor
 
-@BindingAdapter("color")
+@BindingAdapter("initialColor", "finalColor", "opacityColor")
 fun setBackground(
     view: ConstraintLayout,
-    color: String?,
+    initialColor: String?,
+    finalColor: String?,
+    opacityColor: Int?
 ) {
 
-    val backgroundColor = ContextCompat.getColor(
-        view.context, color?.toOceanColor() ?: R.color.ocean_color_brand_primary_pure
+    val init = ContextCompat.getColor(
+        view.context,
+        initialColor?.toOceanColor() ?: R.color.ocean_color_interface_light_pure
     )
 
-    view.setBackgroundColor(backgroundColor)
+    val final = ContextCompat.getColor(
+        view.context,
+        finalColor?.toOceanColor() ?: R.color.ocean_color_interface_light_pure
+    )
+
+    val gradient = GradientDrawable()
+
+    if (initialColor != null && finalColor != null) {
+        gradient.colors = intArrayOf(
+            init, final
+        )
+    } else if (initialColor != null) {
+        gradient.colors = intArrayOf(
+            init, init
+        )
+    }
+
+    gradient.gradientType = GradientDrawable.LINEAR_GRADIENT
+    gradient.shape = GradientDrawable.RECTANGLE
+    gradient.orientation = GradientDrawable.Orientation.LEFT_RIGHT
+    gradient.alpha = opacityColor ?: 255
+    view.background = gradient
 }

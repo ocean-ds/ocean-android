7022a8bad0250926234940524bbf5b4295e5d943
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-08-03 13:30:58 -0300

    feat: finish OceanStatusListItem

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanStatusListItem.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanStatusListItem.kt
@@ -1,6 +1,7 @@
 package br.com.useblu.oceands.components.compose
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,10 +10,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,37 +29,64 @@ import br.com.useblu.oceands.ui.compose.OceanTextStyle
 @Preview
 @Composable
 fun OceanStatusListItemPreview() {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(10.dp),
-        modifier = Modifier.padding(20.dp)
-    ) {
-        OceanStatusListItem(
-            title = "Title",
-            description = "Description",
-            caption = "Caption",
-            tagLabel = "Tag",
-            tagType = OceanTagType.Warning
-        )
+    MaterialTheme {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            modifier = Modifier.padding(20.dp)
+        ) {
+            OceanStatusListItem(
+                title = "Title",
+                description = "Description",
+                caption = "Caption",
+                tagLabel = "Tag",
+                tagType = OceanTagType.Warning,
+                onClick = {
+                    println("click container")
+                }
+            )
 
-        OceanStatusListItem(
-            title = "Title",
-            description = "Description",
-            caption = "Caption",
-            tagLabel = "Tag",
-            tagType = OceanTagType.Warning,
-            tagPosition = OceanStatusListItemTagPosition.RIGHT,
-            rightIconType = OceanStatusListItemRightIconType.CONTEXT_MENU
-        )
+            OceanStatusListItem(
+                title = "Title",
+                description = "Description",
+                caption = "Caption",
+                tagLabel = "Tag",
+                tagType = OceanTagType.Warning,
+                tagPosition = OceanStatusListItemTagPosition.RIGHT,
+                rightIconType = OceanStatusListItemRightIconType.CONTEXT_MENU,
+                onClick = {
+                    println("click container")
+                },
+                onClickRightIcon = {
+                    println("click right icon")
+                }
+            )
 
-        OceanStatusListItem(
-            title = "Title",
-            description = "Description",
-            caption = "Caption",
-            tagLabel = "Tag",
-            tagType = OceanTagType.Warning,
-            tagPosition = OceanStatusListItemTagPosition.RIGHT,
-            rightIconType = OceanStatusListItemRightIconType.NONE
-        )
+            OceanStatusListItem(
+                title = "Title",
+                description = "Description",
+                caption = "Caption",
+                tagLabel = "Tag",
+                tagType = OceanTagType.Warning,
+                tagPosition = OceanStatusListItemTagPosition.RIGHT,
+                isReadOnly = true
+            )
+
+            OceanStatusListItem(
+                title = "Title",
+                description = "Description",
+                caption = "Caption",
+                tagLabel = "Tag",
+                tagType = OceanTagType.Warning,
+                tagPosition = OceanStatusListItemTagPosition.RIGHT,
+                isInactive = true,
+                onClick = {
+                    println("click container")
+                },
+                onClickRightIcon = {
+                    println("click right icon")
+                }
+            )
+        }
     }
 }
 
@@ -71,10 +101,15 @@ fun OceanStatusListItem(
     isReadOnly: Boolean = false,
     isInactive: Boolean = false,
     rightIconType: OceanStatusListItemRightIconType = OceanStatusListItemRightIconType.CHEVRON,
+    onClick: (() -> Unit)? = null,
     onClickRightIcon: (() -> Unit)? = null
 ) {
     Row(
         modifier = Modifier
+            .clickable(
+                enabled = !isReadOnly && !isInactive && onClick != null,
+                onClick = { onClick?.invoke() }
+            )
             .background(color = OceanColors.interfaceLightPure)
             .padding(16.dp)
             .fillMaxWidth(),
@@ -86,18 +121,20 @@ fun OceanStatusListItem(
             Text(
                 text = title,
                 style = OceanTextStyle.paragraph,
-                color = OceanColors.interfaceDarkPure
+                color = if (isInactive) OceanColors.interfaceDarkUp else OceanColors.interfaceDarkPure
             )
             Text(
                 text = description,
-                style = OceanTextStyle.description
+                style = OceanTextStyle.description,
+                color = if (isInactive) OceanColors.interfaceDarkUp else Color.Unspecified
             )
 
             OceanSpacing.StackXXXS()
 
             Text(
                 text = caption,
-                style = OceanTextStyle.caption
+                style = OceanTextStyle.caption,
+                color = if (isInactive) OceanColors.interfaceDarkUp else Color.Unspecified
             )
 
             if (tagPosition == OceanStatusListItemTagPosition.BOTTOM) {
@@ -110,24 +147,34 @@ fun OceanStatusListItem(
             OceanTag(label = tagLabel, type = tagType)
         }
 
-        val rightIcon = when (rightIconType) {
-            OceanStatusListItemRightIconType.CHEVRON -> {
-                R.drawable.icon_chevron_right
+        if (!isReadOnly) {
+            val rightIcon = when (rightIconType) {
+                OceanStatusListItemRightIconType.CHEVRON -> {
+                    R.drawable.icon_chevron_right
+                }
+                OceanStatusListItemRightIconType.CONTEXT_MENU -> {
+                    R.drawable.icon_vertical_solid_dots
+                }
             }
-            OceanStatusListItemRightIconType.CONTEXT_MENU -> {
-                R.drawable.icon_vertical_solid_dots
-            }
-            OceanStatusListItemRightIconType.NONE ->  null
-        }
 
-        rightIcon?.let {
-            OceanSpacing.StackXS()
-            IconButton(
-                onClick = { onClickRightIcon?.invoke() },
-                modifier = Modifier.size(32.dp)
-            ) {
+            val isIconClickable = onClickRightIcon != null && !isInactive
+
+            if (isIconClickable) {
+                OceanSpacing.StackXS()
+                IconButton(
+                    onClick = { onClickRightIcon?.invoke() },
+                    modifier = Modifier.size(32.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(id = rightIcon),
+                        contentDescription = null,
+                        modifier = Modifier.size(20.dp),
+                        tint = OceanColors.interfaceDarkUp
+                    )
+                }
+            } else {
                 Icon(
-                    painter = painterResource(id = it),
+                    painter = painterResource(id = rightIcon),
                     contentDescription = null,
                     modifier = Modifier.size(20.dp),
                     tint = OceanColors.interfaceDarkUp
@@ -143,7 +190,6 @@ enum class OceanStatusListItemTagPosition {
 }
 
 enum class OceanStatusListItemRightIconType {
-    NONE,
     CHEVRON,
     CONTEXT_MENU
 }

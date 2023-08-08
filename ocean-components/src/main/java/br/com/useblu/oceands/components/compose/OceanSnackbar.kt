73ea41375bfd9a0f45ffc3c6c0d666be8b615928
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-08-08 11:53:56 -0300

    feat: create snackbar component

---
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanSnackbar.kt
@@ -0,0 +1,158 @@
+package br.com.useblu.oceands.components.compose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.useblu.oceands.ui.compose.OceanColors
+import br.com.useblu.oceands.ui.compose.OceanFontFamily
+import br.com.useblu.oceands.ui.compose.OceanSpacing
+
+
+@Preview
+@Composable
+fun OceanSnackBarPreview() {
+    Row(
+        modifier = Modifier
+            .background(color = OceanColors.interfaceLightPure)
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.weight(1f)
+        ) {
+            OceanSnackBar(
+                type = OceanSnackBarType.Information
+            )
+
+            OceanSnackBar(
+                type = OceanSnackBarType.Positive
+            )
+
+            OceanSnackBar(
+                type = OceanSnackBarType.Warning
+            )
+
+            OceanSnackBar(
+                type = OceanSnackBarType.Negative
+            )
+        }
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.weight(1f)
+        ) {
+            OceanSnackBar(
+                type = OceanSnackBarType.Information,
+                action = {
+                    println("Action")
+                },
+                actionText = "Action"
+            )
+
+            OceanSnackBar(
+                type = OceanSnackBarType.Positive,
+                action = {
+                    println("Action")
+                },
+                actionText = "Action"
+            )
+
+            OceanSnackBar(
+                type = OceanSnackBarType.Warning,
+                action = {
+                    println("Action")
+                },
+                actionText = "Action"
+            )
+
+            OceanSnackBar(
+                type = OceanSnackBarType.Negative,
+                action = {
+                    println("Action")
+                },
+                actionText = "Action"
+            )
+        }
+    }
+}
+
+@Composable
+fun OceanSnackBar(
+    type: OceanSnackBarType,
+    action: (() -> Unit)? = null,
+    actionText: String? = null
+) {
+    val actionComposable: @Composable (() -> Unit)? = if (action != null && actionText != null) {
+        {
+            TextButton(onClick = { action() }) {
+                Text(
+                    text = actionText,
+                    fontFamily = OceanFontFamily.BaseBold,
+                    fontSize = 14.sp,
+                    color = type.getColor()
+                )
+            }
+        }
+    } else null
+
+    Snackbar(
+        modifier = Modifier,
+        action = actionComposable
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            OceanIcon(
+                token = type.getIcon(),
+                tint = type.getColor()
+            )
+
+            OceanSpacing.StackXS()
+
+            Text(
+                text = "Text",
+                fontFamily = OceanFontFamily.BaseRegular,
+                fontSize = 14.sp,
+                color = OceanColors.interfaceLightPure
+            )
+        }
+    }
+}
+
+
+sealed interface OceanSnackBarType {
+    fun getIcon(): String {
+        return when (this) {
+            is Information -> "infooutline"
+            is Positive -> "checkcircleoutline"
+            is Warning -> "exclamationcircleoutline"
+            is Negative -> "xcircleoutline"
+        }
+    }
+
+    @Composable
+    fun getColor(): Color {
+        return when (this) {
+            is Information -> OceanColors.brandPrimaryUp
+            is Positive -> OceanColors.statusPositivePure
+            is Warning -> OceanColors.statusWarningPure
+            is Negative -> OceanColors.statusNegativePure
+        }
+    }
+    object Information : OceanSnackBarType
+    object Positive : OceanSnackBarType
+    object Warning : OceanSnackBarType
+    object Negative : OceanSnackBarType
+}

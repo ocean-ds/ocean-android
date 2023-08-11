174c5c6ed220e544447903ec806fd4a29a487eed
Tarik (Julio Martins) <38379044+nordael@users.noreply.github.com>
2023-08-11 15:06:46 -0300

    refactor: replace string for ocean icon type to name ocean icons

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanIcon.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanIcon.kt
@@ -7,16 +7,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.useblu.oceands.model.OceanIconType
+import br.com.useblu.oceands.ui.compose.OceanColors
 import br.com.useblu.oceands.utils.toOceanIcon
+
+@Preview(showBackground = true)
+@Composable
+fun OceanIconPreview() {
+    OceanIcon(
+        iconType = OceanIconType.SHIELD_CHECK_OUTLINE,
+        tint = OceanColors.brandPrimaryPure,
+        fallbackIcon = OceanIconType.EXCLAMATION_CIRCLE_OUTLINE
+    )
+}
 
 @Composable
 fun OceanIcon(
-    token: String,
+    iconType: OceanIconType,
     modifier: Modifier = Modifier,
     tint: Color = LocalContentColor.current,
-    fallbackIcon: String? = null
+    fallbackIcon: OceanIconType? = null
 ) {
-    val icon = token.toOceanIcon()
+    val icon = iconType.toOceanIcon()
     val fallback = fallbackIcon?.toOceanIcon()
 
     if (icon != null) {

943aa34ac1b93ab9efa923002429fd4e071e2e82
Fernando Zanutto <fernando.zanutto@useblu.com.br>
2023-08-11 01:23:40 -0300

    feat: refactor ocean bottom sheet compose (#421)

--- ocean-components/src/main/java/br/com/useblu/oceands/components/OceanBottomSheetCompose.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/OceanBottomSheetCompose.kt
@@ -1,34 +1,36 @@
 package br.com.useblu.oceands.components
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import br.com.useblu.oceands.R
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.useblu.oceands.components.compose.OceanButton
 import br.com.useblu.oceands.databinding.OceanBottomSheetComposeBinding
+import br.com.useblu.oceands.ui.compose.OceanButtonStyle
+import br.com.useblu.oceands.ui.compose.OceanSpacing
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class OceanBottomSheetCompose(
-    private val context: Context
-): BottomSheetDialogFragment() {
+class OceanBottomSheetCompose: BottomSheetDialogFragment() {
 
     private var code: String? = null
-    private var orientationButtons: Int = Orientation.Horizontal.direction
+    private var orientationButtons: Orientation = Orientation.Horizontal
     private var isDismiss: Boolean = true
-    private var textPositive: String? = context.getString(R.string.ok)
+    private var textPositive: String = ""
     private var textNegative: String? = null
-    private var actionPositive: (() -> Unit?)? = null
-    private var actionNegative: (() -> Unit?)? = null
+    private var actionPositive: (() -> Unit) = { }
+    private var actionNegative: (() -> Unit)? = null
     private var isCritical: Boolean = false
-    private var composeContent: @Composable (() -> Unit)? = null
+    private var composeContent: @Composable (ColumnScope.() -> Unit) = {}
     private lateinit var binding: OceanBottomSheetComposeBinding
 
     override fun onCreateView(
@@ -37,19 +39,6 @@ class OceanBottomSheetCompose(
         savedInstanceState: Bundle?
     ): View {
         binding = OceanBottomSheetComposeBinding.inflate(inflater, container, false)
-
-        textPositive?.let {
-            binding.textPositive = it
-        }
-
-        binding.clickPositive = {
-            dismiss()
-            actionPositive?.let { it() }
-        }
-
-        textNegative?.let {
-            binding.textNegative = it
-        }
 
         binding.clickNegative = {
             dismiss()
@@ -60,29 +49,39 @@ class OceanBottomSheetCompose(
             binding.code = it
         }
 
-        composeContent?.let {
-            binding.composeView.setContent(it)
+        binding.composeView.setContent {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                composeContent()
+
+                BottomButtons(
+                    positiveLabel = textPositive,
+                    positiveAction = {
+                        actionPositive.invoke()
+                        dismiss()
+                    },
+                    negativeLabel = textNegative,
+                    negativeAction = {
+                        actionNegative?.invoke()
+                        dismiss()
+                    },
+                    orientation = orientationButtons
+                )
+            }
+
         }
 
         binding.cancelabled = isDismiss
-        binding.isCritical = isCritical
-        binding.orientationButtons = orientationButtons
-
         isCancelable = isDismiss
 
         return binding.root
     }
 
     fun withComposeContent(content: @Composable ColumnScope.() -> Unit): OceanBottomSheetCompose {
-        composeContent = {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                content()
-            }
-        }
+        composeContent = content
 
         return this
     }
@@ -98,18 +97,18 @@ class OceanBottomSheetCompose(
     }
 
     fun withOrientationButtons(orientation: Orientation): OceanBottomSheetCompose {
-        this.orientationButtons = orientation.direction
+        this.orientationButtons = orientation
         return this
     }
 
-    fun withActionPositive(text: Int, callBack: () -> Unit): OceanBottomSheetCompose {
-        this.textPositive = context.getString(text)
+    fun withActionPositive(text: String, callBack: () -> Unit): OceanBottomSheetCompose {
+        this.textPositive = text
         this.actionPositive = callBack
         return this
     }
 
-    fun withActionNegative(text: Int, callBack: () -> Unit): OceanBottomSheetCompose {
-        this.textNegative = context.getString(text)
+    fun withActionNegative(text: String, callBack: () -> Unit): OceanBottomSheetCompose {
+        this.textNegative = text
         this.actionNegative = callBack
         return this
     }
@@ -119,8 +118,76 @@ class OceanBottomSheetCompose(
         return this
     }
 
-    sealed class Orientation(val direction: Int) {
-        object Horizontal : Orientation(direction = LinearLayout.HORIZONTAL)
-        object Vertical : Orientation(direction = LinearLayout.VERTICAL)
+    enum class Orientation {
+        Horizontal, Vertical
+    }
+}
+
+
+@Preview
+@Composable
+private fun BottomButtonsPreview() {
+    BottomButtons(
+        positiveLabel = "Teste",
+        positiveAction = {},
+        negativeLabel = "Cancelar",
+        negativeAction = {},
+        orientation = OceanBottomSheetCompose.Orientation.Horizontal,
+        isCritical = true
+    )
+}
+
+@Composable
+private fun BottomButtons(
+    positiveLabel: String,
+    positiveAction: () -> Unit,
+    negativeLabel: String? = null,
+    negativeAction: (() -> Unit)? = null,
+    isCritical: Boolean = false,
+    orientation: OceanBottomSheetCompose.Orientation = OceanBottomSheetCompose.Orientation.Horizontal
+) {
+
+    val buttons: @Composable (Modifier) -> Unit = {
+        val primaryStyle = if (isCritical) {
+            OceanButtonStyle.PrimaryCriticalMedium
+        } else OceanButtonStyle.PrimaryMedium
+
+        OceanButton(
+            text = positiveLabel,
+            buttonStyle = primaryStyle,
+            onClick = { positiveAction.invoke() },
+            modifier = it
+        )
+
+        if (negativeLabel != null && negativeAction != null) {
+            OceanSpacing.StackXS()
+
+            OceanButton(
+                text = negativeLabel,
+                buttonStyle = OceanButtonStyle.SecondaryMedium,
+                onClick = { negativeAction.invoke()},
+                modifier = it
+            )
+        }
+    }
+
+    val modifier = Modifier
+        .padding(top = 24.dp)
+
+    when (orientation) {
+        OceanBottomSheetCompose.Orientation.Horizontal -> {
+            Row(
+                modifier = modifier
+            ) {
+                buttons(Modifier.weight(1f))
+            }
+        }
+        OceanBottomSheetCompose.Orientation.Vertical -> {
+            Column(
+                modifier = modifier
+            ) {
+                buttons(Modifier.fillMaxWidth())
+            }
+        }
     }
 }

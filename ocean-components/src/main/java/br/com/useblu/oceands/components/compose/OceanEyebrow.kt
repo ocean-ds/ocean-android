c96f5c800a9665761c1abd8d8a4320461164d7b0
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-08-17 11:44:22 -0300

    feat: creates eyebrow component

---
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanEyebrow.kt
@@ -0,0 +1,53 @@
+package br.com.useblu.oceands.components.compose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.useblu.oceands.ui.compose.OceanColors
+import br.com.useblu.oceands.ui.compose.OceanFontFamily
+
+@Preview
+@Composable
+fun OceanEyebrowPreview() {
+    Column(
+        modifier = Modifier
+            .background(color = OceanColors.interfaceLightPure)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        OceanEyebrow(
+            text = "O link blu voltou"
+        )
+
+        OceanEyebrow(
+            text = "eyebrow"
+        )
+    }
+}
+
+@Composable
+fun OceanEyebrow(
+    text: String,
+    modifier: Modifier = Modifier,
+    textColor: Color = OceanColors.interfaceDarkDeep
+) {
+    Text(
+        text = text.uppercase(),
+        style = TextStyle(
+            fontSize = 12.sp,
+            fontFamily = OceanFontFamily.BaseMedium,
+            color = textColor,
+            letterSpacing = 2.16.sp,
+        ),
+        modifier = modifier
+    )
+}

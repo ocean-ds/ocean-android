ac38504060f78f75446c52a92b4e41f711162bff
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-08-30 12:29:32 -0300

    feat: shortcut list and layout done (todo: badge)

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanShortcut.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanShortcut.kt
@@ -1,14 +1,19 @@
 package br.com.useblu.oceands.components.compose
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,39 +23,131 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.useblu.oceands.model.OceanBadgeType
 import br.com.useblu.oceands.ui.compose.OceanColors
+import br.com.useblu.oceands.ui.compose.OceanSpacing
 import br.com.useblu.oceands.ui.compose.OceanTextStyle
 import br.com.useblu.oceands.utils.OceanIcons
 
 @Preview
 @Composable
 fun OceanShortcutPreview() {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        item {
-            OceanShortcut(
-                label = "Label",
-                icon = OceanIcons.ACADEMIC_CAP_SOLID,
-            )
-        }
+    val models = listOf(
+        OceanShortcutModel(
+            label = "Tiny Vertical",
+            icon = OceanIcons.ACADEMIC_CAP_SOLID
+        ),
+        OceanShortcutModel(
+            label = "Tiny Vertical",
+            icon = OceanIcons.ACADEMIC_CAP_SOLID
+        ),
+        OceanShortcutModel(
+            label = "TinyHorizontal",
+            icon = OceanIcons.ACADEMIC_CAP_SOLID,
+            layout = OceanShortcutLayout.TinyHorizontal
+        ),
+        OceanShortcutModel(
+            label = "TinyHorizontal",
+            icon = OceanIcons.ACADEMIC_CAP_SOLID,
+            layout = OceanShortcutLayout.TinyHorizontal
+        ),
+        OceanShortcutModel(
+            label = "Label",
+            icon = OceanIcons.ACADEMIC_CAP_SOLID,
+            blocked = true
+        ),
+        OceanShortcutModel(
+            label = "Label",
+            icon = OceanIcons.ACADEMIC_CAP_SOLID,
+            blocked = true,
+            layout = OceanShortcutLayout.TinyHorizontal
+        ),
+        OceanShortcutModel(
+            label = "Label",
+            icon = OceanIcons.ACADEMIC_CAP_SOLID,
+            layout = OceanShortcutLayout.Small
+        ),
+        OceanShortcutModel(
+            label = "Label",
+            icon = OceanIcons.ACADEMIC_CAP_SOLID,
+            layout = OceanShortcutLayout.Small
+        ),
+        OceanShortcutModel(
+            label = "Label",
+            icon = OceanIcons.ACADEMIC_CAP_SOLID,
+            layout = OceanShortcutLayout.Small
+        ),
+        OceanShortcutModel(
+            label = "Label",
+            icon = OceanIcons.ACADEMIC_CAP_SOLID,
+            layout = OceanShortcutLayout.MediumHorizontal,
+            description = "Lorem ipsum dolor sit amet, consectetur."
+        ),
+        OceanShortcutModel(
+            label = "Label",
+            icon = OceanIcons.ACADEMIC_CAP_SOLID,
+            layout = OceanShortcutLayout.MediumVertical,
+            description = "Lorem ipsum dolor sit amet, consectetur.",
+            disabled = true
+        ),
+        OceanShortcutModel(
+            label = "Label",
+            icon = OceanIcons.ACADEMIC_CAP_SOLID,
+            layout = OceanShortcutLayout.MediumVertical,
+            description = "Lorem ipsum dolor sit amet, consectetur.",
+            disabled = true
+        ),
+    )
 
-        item {
-            OceanShortcut(
-                label = "Label",
-                icon = OceanIcons.ACADEMIC_CAP_SOLID,
-            )
-        }
+    OceanShortcutList(
+        models = models,
+        columns = 2
+    )
+}
 
-        item {
-            OceanShortcut(
-                label = "Label",
-                icon = OceanIcons.ACADEMIC_CAP_SOLID,
-                blocked = true
-            )
+@Composable
+fun OceanShortcutList(
+    models: List<OceanShortcutModel>,
+    columns: Int
+) {
+    Column {
+        for (i in models.indices step columns) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.height(IntrinsicSize.Min)
+            ) {
+                for (j in 0 until columns) {
+                    if (i + j < models.size) {
+                        OceanShortcut(
+                            model = models[i + j],
+                            modifier = Modifier.weight(1f)
+                        )
+                    } else {
+                        Spacer(modifier = Modifier.weight(1f))
+                    }
+                }
+            }
+
+            OceanSpacing.StackXS()
         }
     }
+}
+
+@Composable
+fun OceanShortcut(
+    model: OceanShortcutModel,
+    modifier: Modifier = Modifier
+) {
+    OceanShortcut(
+        label = model.label,
+        icon = model.icon,
+        modifier = modifier,
+        description = model.description,
+        count = model.count,
+        badgeType = model.badgeType,
+        action = model.action,
+        layout = model.layout,
+        blocked = model.blocked,
+        disabled = model.disabled
+    )
 }
 
 @Composable
@@ -58,49 +155,93 @@ fun OceanShortcut(
     label: String,
     icon: OceanIcons,
     modifier: Modifier = Modifier,
-    count: String? = null,
     description: String? = null,
+    count: String? = null,
     badgeType: OceanBadgeType? = null,
-    action: () -> Unit = {},
+    action: (() -> Unit)? = null,
     layout: OceanShortcutLayout = OceanShortcutLayout.TinyVertical,
-    blocked: Boolean = false
+    blocked: Boolean = false,
+    disabled: Boolean = false
 ) {
+    val backgroundColor = if (disabled) {
+        OceanColors.interfaceLightDown
+    } else OceanColors.interfaceLightPure
+
+    val iconColor = if (disabled) {
+        OceanColors.interfaceDarkUp
+    } else OceanColors.brandPrimaryDown
+
+    val titleColor = if (disabled) {
+        OceanColors.interfaceDarkUp
+    } else OceanColors.interfaceDarkDeep
+
+    val borderColor = if (disabled) {
+        OceanColors.interfaceLightDown
+    } else OceanColors.interfaceLightDown
 
     Box(
         modifier = modifier
             .background(
-                color = OceanColors.interfaceLightPure,
+                color = backgroundColor,
                 shape = RoundedCornerShape(8.dp)
             )
-            .clickable {
-                action()
-            }
+            .border(
+                width = 1.dp,
+                color = borderColor,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .clickable(
+                enabled = !disabled && !blocked && action != null,
+                onClick = { action?.invoke() }
+            )
+            .fillMaxHeight()
     ) {
         if (blocked) {
             OceanIcon(
                 iconType = OceanIcons.LOCK_CLOSED_SOLID,
                 modifier = Modifier
-                    .size(16.dp)
                     .padding(top = 8.dp, end = 8.dp)
-                    .align(Alignment.TopEnd)
+                    .size(16.dp)
+                    .align(Alignment.TopEnd),
+                tint = OceanColors.interfaceDarkUp
             )
         }
 
         Column(
             modifier = Modifier
                 .padding(16.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
-            OceanIcon(
-                iconType = icon,
-                tint = OceanColors.brandPrimaryDown,
-                modifier = Modifier.size(24.dp)
-            )
+            layout.GetTextIconLayout {
+                OceanIcon(
+                    iconType = icon,
+                    tint = iconColor,
+                    modifier = Modifier.size(24.dp)
+                )
 
-            Text(
-                text = label,
-                style = OceanTextStyle.heading5,
-                color = OceanColors.interfaceDarkDown
-            )
+                layout.GetSpacer()
+
+                Text(
+                    text = label,
+                    style = OceanTextStyle.heading5,
+                    color = titleColor
+                )
+            }
+
+            if (layout.canShowDescription() && description != null) {
+                val descriptionColor = if (disabled) {
+                    OceanColors.interfaceDarkUp
+                } else OceanColors.interfaceDarkDown
+
+                OceanSpacing.StackXXS()
+
+                Text(
+                    text = description,
+                    style = OceanTextStyle.caption,
+                    color = descriptionColor
+                )
+            }
         }
     }
 }
@@ -112,5 +253,47 @@ enum class OceanShortcutLayout {
     MediumVertical,
     MediumHorizontal;
 
+    @Composable
+    internal fun GetSpacer() {
+        when (this) {
+            TinyVertical,
+            TinyHorizontal -> OceanSpacing.StackXXS()
+            Small -> Spacer(modifier = Modifier.size(30.dp))
+            MediumVertical -> OceanSpacing.StackXS()
+            MediumHorizontal -> OceanSpacing.StackXXS()
+        }
+    }
 
+    @Composable
+    internal fun GetTextIconLayout(content: @Composable () -> Unit) {
+        when (this) {
+            TinyVertical,
+            Small,
+            MediumVertical -> {
+                Column {
+                    content()
+                }
+            }
+            TinyHorizontal,
+            MediumHorizontal -> {
+                Row {
+                    content()
+                }
+            }
+        }
+    }
+
+    fun canShowDescription() = this == MediumHorizontal || this == MediumVertical
 }
+
+data class OceanShortcutModel(
+    val label: String,
+    val description: String? = null,
+    val icon: OceanIcons,
+    val count: String? = null,
+    val badgeType: OceanBadgeType? = null,
+    val action: (() -> Unit)? = null,
+    val layout: OceanShortcutLayout = OceanShortcutLayout.TinyVertical,
+    val blocked: Boolean = false,
+    val disabled: Boolean = false
+)

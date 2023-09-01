c17adf3f86c9f0eec2e6ac8c7088a57a030d72f1
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-09-01 10:22:40 -0300

    feat: add show progress to Card Cross Sell

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanCardCrossSell.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanCardCrossSell.kt
@@ -5,13 +5,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -51,6 +54,7 @@ fun OceanCardCrossSellPreview() {
             title = "Boleto Mais Seguro",
             description = "Menos atraso e inadimplência para vendas feitas com boleto",
             actionTitle = "Saiba mais",
+            showProgress = true,
             //imageUrl = "https://portal-cicloentrada.blu.com.br/assets/icons/ilustra-crosssel-bms-2-03c05d6bf3ce58709f5fd75b890cc2eca1bd8beb5806a2e7be37fb38b512953e.png"
         )
     }
@@ -133,6 +137,7 @@ fun OceanCardCrossSell(
 
         Row(
             verticalAlignment = Alignment.CenterVertically,
+
             modifier = Modifier
                 .background(
                     color = OceanColors.interfaceLightPure,
@@ -143,26 +148,36 @@ fun OceanCardCrossSell(
                     color = OceanColors.interfaceLightDown,
                     shape = RoundedCornerShape(bottomStart = 8.dp, bottomEnd = 8.dp)
                 )
+                .fillMaxWidth()
+                .padding(vertical = 14.dp),
+            horizontalArrangement = Arrangement.Center
 
         ) {
-            Text(
-                text = actionTitle,
-                color = OceanColors.brandPrimaryPure,
-                fontFamily = OceanFontFamily.BaseBold,
-                fontSize = 14.sp,
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(horizontal = 16.dp)
-            )
+            if (!showProgress) {
+                Text(
+                    text = actionTitle,
+                    color = OceanColors.brandPrimaryPure,
+                    fontFamily = OceanFontFamily.BaseBold,
+                    fontSize = 14.sp,
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(horizontal = 16.dp)
+                )
 
-            OceanIcon(
-                iconType = actionIcon,
-                tint = OceanColors.brandPrimaryPure,
-                modifier = Modifier
-                    .padding(end = 8.dp)
-                    .padding(vertical = 14.dp)
-                    .size(20.dp)
-            )
+                OceanIcon(
+                    iconType = actionIcon,
+                    tint = OceanColors.brandPrimaryPure,
+                    modifier = Modifier
+                        .padding(end = 8.dp)
+                        .size(20.dp)
+                )
+            } else {
+                CircularProgressIndicator(
+                    color = OceanColors.brandPrimaryPure,
+                    modifier = Modifier.size(20.dp),
+                    strokeWidth = 2.dp
+                )
+            }
         }
     }
 }

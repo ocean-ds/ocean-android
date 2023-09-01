19f497db0e6cd129b6cf629ea2b73f3b38193725
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-09-01 10:07:05 -0300

    feat: add missing parameters and adjust image padding and content scale

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanCardCrossSell.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanCardCrossSell.kt
@@ -4,6 +4,7 @@ import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,14 +16,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.com.useblu.oceands.ui.compose.OceanColors
 import br.com.useblu.oceands.ui.compose.OceanFontFamily
+import br.com.useblu.oceands.ui.compose.OceanSpacing
 import br.com.useblu.oceands.ui.compose.OceanTextStyle
 import br.com.useblu.oceands.utils.OceanIcons
+import com.skydoves.landscapist.ImageOptions
 import com.skydoves.landscapist.glide.GlideImage
 
 @Preview
@@ -34,9 +39,19 @@ fun OceanCardCrossSellPreview() {
             .padding(16.dp)
     ) {
         OceanCardCrossSell(
-            title = "Title",
-            description = "Description",
-            actionTitle = "Call to action"
+            title = "Boleto Mais Seguro",
+            description = "Menos atraso e inadimplência para vendas feitas com boleto",
+            actionTitle = "Saiba mais",
+            //imageUrl = "https://portal-cicloentrada.blu.com.br/assets/icons/ilustra-crosssel-bms-2-03c05d6bf3ce58709f5fd75b890cc2eca1bd8beb5806a2e7be37fb38b512953e.png"
+        )
+
+        Spacer(modifier = Modifier.size(16.dp))
+
+        OceanCardCrossSell(
+            title = "Boleto Mais Seguro",
+            description = "Menos atraso e inadimplência para vendas feitas com boleto",
+            actionTitle = "Saiba mais",
+            //imageUrl = "https://portal-cicloentrada.blu.com.br/assets/icons/ilustra-crosssel-bms-2-03c05d6bf3ce58709f5fd75b890cc2eca1bd8beb5806a2e7be37fb38b512953e.png"
         )
     }
 }
@@ -46,13 +61,20 @@ fun OceanCardCrossSell(
     title: String,
     description: String,
     actionTitle: String,
+    modifier: Modifier = Modifier,
     imageUrl: String? = null,
-    @DrawableRes imageResource: Int? = null
+    @DrawableRes imageResource: Int? = null,
+    actionIcon: OceanIcons = OceanIcons.CHEVRON_RIGHT_SOLID,
+    titleColor: Color = Color.Unspecified,
+    descriptionColor: Color = Color.Unspecified,
+    backgroundColor: Color = OceanColors.interfaceLightUp,
+    showProgress: Boolean = false,
+    onClick: (() -> Unit)? = null
 ) {
     Column(
-        modifier = Modifier
+        modifier = modifier
             .background(
-                color = OceanColors.interfaceLightUp,
+                color = backgroundColor,
                 shape = RoundedCornerShape(8.dp)
             )
             .border(
@@ -60,9 +82,13 @@ fun OceanCardCrossSell(
                 color = OceanColors.interfaceLightDown,
                 shape = RoundedCornerShape(8.dp)
             )
+            .clickable(
+                enabled = !showProgress && onClick != null,
+                onClick = { onClick?.invoke() }
+            )
     ){
         Row(
-            modifier = Modifier.padding(horizontal = 16.dp),
+            modifier = Modifier.padding(16.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Column(
@@ -70,11 +96,16 @@ fun OceanCardCrossSell(
             ) {
                 Text(
                     text = title,
-                    style = OceanTextStyle.heading4
+                    style = OceanTextStyle.heading4,
+                    color = titleColor
                 )
+
+                OceanSpacing.StackXXXS()
+
                 Text(
                     text = description,
-                    style = OceanTextStyle.description
+                    style = OceanTextStyle.description,
+                    color = descriptionColor
                 )
             }
 
@@ -82,7 +113,11 @@ fun OceanCardCrossSell(
                 imageUrl != null -> {
                     GlideImage(
                         imageModel = { imageUrl },
-                        modifier = Modifier.size(80.dp)
+                        modifier = Modifier.size(80.dp),
+                        imageOptions = ImageOptions(
+                            contentScale = ContentScale.Fit,
+                            alignment = Alignment.Center
+                        )
                     )
                 }
                 imageResource != null -> {
@@ -121,7 +156,7 @@ fun OceanCardCrossSell(
             )
 
             OceanIcon(
-                iconType = OceanIcons.CHEVRON_RIGHT_SOLID,
+                iconType = actionIcon,
                 tint = OceanColors.brandPrimaryPure,
                 modifier = Modifier
                     .padding(end = 8.dp)

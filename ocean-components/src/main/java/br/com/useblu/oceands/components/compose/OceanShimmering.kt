32bfdf1761e405fc9a25b22a048d229430025440
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-09-11 16:16:49 -0300

    feat: add previews and Skeleton for Status List Item

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanShimmering.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanShimmering.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,18 +16,60 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import br.com.useblu.oceands.components.compose.skeleton.OceanStatusListItemSkeleton
 import br.com.useblu.oceands.ui.compose.OceanColors
+import kotlinx.coroutines.delay
 
 
 @Preview
 @Composable
-fun OceanShimmeringPreview(){
+fun PreviewSkeleton() {
+    var showSkeleton by remember { mutableStateOf(true) }
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        LaunchedEffect(key1 = true) {
+            delay(2000)
+            showSkeleton = false
+        }
+
+        if (showSkeleton) {
+            ScreenSkeleton()
+        } else {
+            OceanStatusListItem(title = "Item 1")
+            OceanStatusListItem(title = "Item 2")
+            OceanStatusListItem(title = "Item 3")
+            OceanStatusListItem(title = "Item 4")
+            OceanStatusListItem(title = "Item 5")
+            OceanStatusListItem(title = "Item 6")
+        }
+
+    }
+}
+
+@Composable
+private fun ScreenSkeleton() {
+    repeat(5) {
+        OceanStatusListItemSkeleton()
+    }
+}
+
+@Preview
+@Composable
+fun OceanShimmeringPreview() {
     OceanShimmering { brush ->
         Column(
             modifier = Modifier

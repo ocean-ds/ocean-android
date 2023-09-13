7a1403dbf876d5c597aefa5971754d8499fe235a
Fernando Zanutto <fernando.zanutto@useblu.com.br>
2023-09-13 13:03:19 -0300

    Feat/add extension to convert html to annotated string (#450)
    * feat: add extension to convert from html string to annotated string
    
    * feat: add modifier parameter

--- ocean-components/src/main/java/br/com/useblu/oceands/extensions/compose/Extensions.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/extensions/compose/Extensions.kt
@@ -1,9 +1,22 @@
 package br.com.useblu.oceands.extensions.compose
 
+import android.graphics.Typeface
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.text.style.UnderlineSpan
 import androidx.compose.foundation.background
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.core.text.HtmlCompat
 import br.com.useblu.oceands.ui.compose.OceanColors
 
 fun Modifier.iconContainerBackground(showBackground: Boolean) = composed {
@@ -14,5 +27,28 @@ fun Modifier.iconContainerBackground(showBackground: Boolean) = composed {
         )
     } else {
         this
+    }
+}
+
+fun String.htmlToAnnotatedString(): AnnotatedString {
+    val spanned = HtmlCompat.fromHtml(this, HtmlCompat.FROM_HTML_MODE_LEGACY)
+    return spanned.toAnnotatedString()
+}
+
+fun Spanned.toAnnotatedString(): AnnotatedString = buildAnnotatedString {
+    val spanned = this@toAnnotatedString
+    append(spanned.toString())
+    getSpans(0, spanned.length, Any::class.java).forEach { span ->
+        val start = getSpanStart(span)
+        val end = getSpanEnd(span)
+        when (span) {
+            is StyleSpan -> when (span.style) {
+                Typeface.BOLD -> addStyle(SpanStyle(fontWeight = FontWeight.Bold), start, end)
+                Typeface.ITALIC -> addStyle(SpanStyle(fontStyle = FontStyle.Italic), start, end)
+                Typeface.BOLD_ITALIC -> addStyle(SpanStyle(fontWeight = FontWeight.Bold, fontStyle = FontStyle.Italic), start, end)
+            }
+            is UnderlineSpan -> addStyle(SpanStyle(textDecoration = TextDecoration.Underline), start, end)
+            is ForegroundColorSpan -> addStyle(SpanStyle(color = Color(span.foregroundColor)), start, end)
+        }
     }
 }

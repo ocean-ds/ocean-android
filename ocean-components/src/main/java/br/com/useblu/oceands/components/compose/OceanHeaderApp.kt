d95bfbdf13f53469ff18011a239ebea51d600c3f
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-09-15 10:21:50 -0300

    feat: add cards to pager and page indicator

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanHeaderApp.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanHeaderApp.kt
@@ -1,19 +1,33 @@
 package br.com.useblu.oceands.components.compose
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.com.useblu.oceands.model.OceanBadgeType
@@ -25,10 +39,10 @@ import br.com.useblu.oceands.ui.compose.OceanFontFamily
 import br.com.useblu.oceands.utils.OceanIcons
 
 
-@Preview(device = "spec:width=1800px,height=2340px,dpi=440,orientation=portrait")
-@Composable
-fun OceanHeaderCardsPreview() {
-    val bluModel = OceanBalanceBluModel(
+private val modelPreview = OceanHeaderAppModel(
+    clientName = "Fcr Colchões",
+    formattedCnpj = "32.677.554/0001-14",
+    balanceBluModel = OceanBalanceBluModel(
         firstLabel = "First Label",
         firstValue = "First Value",
         secondLabel = "Second Label",
@@ -37,42 +51,111 @@ fun OceanHeaderCardsPreview() {
         thirdValue = "Third Value",
         buttonCta = "Extrato",
         buttonDescription = "Confira tudo o que entrou e saiu da sua Conta Digital Blu"
-    )
-    val othersModel = OceanBalanceOthersModel(
+    ),
+    balanceOthersModel = OceanBalanceOthersModel(
         title = "Saldo em Outras maquininhas",
         description = "Receba na Blu as vendas feitas nas suas outras maquininhas",
         buttonCta = "Extrato",
         buttonCtaCollapsed = "Extrato",
         onClickButton = {}
     )
-    Row {
-        OceanBalanceBluCard(model = bluModel, modifier = Modifier.weight(1f))
-        OceanBalanceOthersCard(model = othersModel, modifier = Modifier.weight(1f))
+)
+
+@Preview
+@Composable
+fun OceanHeaderCardsPreview() {
+    Column {
+        OceanBalanceBluCard(model = modelPreview.balanceBluModel)
+        OceanBalanceOthersCard(model = modelPreview.balanceOthersModel)
     }
 }
 
 @Preview
 @Composable
 fun OceanHeaderAppPreview() {
-    val model = OceanHeaderAppModel(
-        clientName = "Fcr Colchões",
-        formattedCnpj = "32.677.554/0001-14"
-    )
     OceanHeaderApp(
-        model
+        modelPreview
     )
 }
 
+@ExperimentalFoundationApi
+private val balancePageSize = object : PageSize {
+    override fun Density.calculateMainAxisPageSize(
+        availableSpace: Int,
+        pageSpacing: Int
+    ): Int {
+        return availableSpace - 24.dp.roundToPx()
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun OceanHeaderApp(
     model: OceanHeaderAppModel,
     modifier: Modifier = Modifier
 ) {
+    val pagerState = rememberPagerState()
+
     Column(
         modifier = modifier
             .background(color = OceanColors.brandPrimaryPure)
     ) {
         Header(model = model)
+
+        HorizontalPager(
+            pageCount = 2,
+            state = pagerState,
+            pageSpacing = 8.dp,
+            contentPadding = PaddingValues(horizontal = 16.dp),
+            beyondBoundsPageCount = 1,
+            pageSize = balancePageSize
+        ) { page ->
+            when (page) {
+                0 -> {
+                    OceanBalanceBluCard(model = model.balanceBluModel)
+                }
+                1 -> {
+                    OceanBalanceOthersCard(model = model.balanceOthersModel)
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.size(8.dp))
+
+        PageIndicator(pagerState = pagerState)
+
+        Spacer(modifier = Modifier.size(16.dp))
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+private fun PageIndicator(pagerState: PagerState) {
+    Row(
+        Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        repeat(2) { iteration ->
+            val isSelected = remember(pagerState.currentPage) {
+                pagerState.currentPage == iteration
+            }
+
+            val color = animateColorAsState(
+                targetValue = if (isSelected) OceanColors.interfaceLightUp else OceanColors.brandPrimaryDown,
+                label = "Page Indicator Color",
+                animationSpec = tween(300)
+            )
+
+            Box(
+                modifier = Modifier
+                    .padding(2.dp)
+                    .clip(CircleShape)
+                    .background(color.value)
+                    .size(4.dp)
+            )
+        }
     }
 }
 

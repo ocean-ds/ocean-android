35c2b9d6d3e6fcb7bf7c4932f4c357c37a8d204e
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-09-19 17:29:59 -0300

    feat: add ocean theme

---
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanTheme.kt
@@ -0,0 +1,24 @@
+package br.com.useblu.oceands.components.compose
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
+import br.com.useblu.oceands.ui.compose.OceanColors
+import br.com.useblu.oceands.ui.compose.OceanFontFamily
+
+@Composable
+fun OceanTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        typography = Typography().copy(
+            bodyLarge = TextStyle(fontFamily = OceanFontFamily.BaseRegular)
+        ),
+        colorScheme = MaterialTheme.colorScheme.copy(
+            background = OceanColors.interfaceLightPure
+        )
+    ) {
+        content()
+    }
+}

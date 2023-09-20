943d84d0edf3947fc3a6116598e863b49faa7ada
Tarik (Julio Martins) <38379044+nordael@users.noreply.github.com>
2023-09-20 11:26:01 -0300

    Feat/implementing ocean alert (#455)
    * feat: creating ocean alert
    
    * fix: adjusting string preview

---
+++ ocean-components/src/main/java/br/com/useblu/oceands/model/compose/OceanAlertType.kt
@@ -0,0 +1,85 @@
+package br.com.useblu.oceands.model.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import br.com.useblu.oceands.ui.compose.OceanColors
+import br.com.useblu.oceands.ui.compose.OceanTextStyle
+import br.com.useblu.oceands.utils.OceanIcons
+
+sealed interface OceanAlertType {
+    class Default(
+        val alertType: AlertStyle = AlertStyle.StyleInfo(),
+        val description: String
+    ): OceanAlertType
+
+    class EntitledShort(
+        val alertType: AlertStyle = AlertStyle.StyleInfo(),
+        val title: String,
+        val description: String
+    ): OceanAlertType
+
+    class EntitledLong(
+        val alertType: AlertStyle = AlertStyle.StyleInfo(),
+        val title: String,
+        val description: String
+    ): OceanAlertType
+
+    class Labeled(
+        val alertType: AlertStyle = AlertStyle.StyleInfo(),
+        val title: String? = null,
+        val description: String,
+        val label: String
+    ): OceanAlertType
+}
+
+sealed interface AlertStyle {
+
+    val titleColor: @Composable () -> Color
+    val titleStyle: @Composable () -> TextStyle
+    val descriptionStyle: @Composable () -> TextStyle
+    val descriptionColor: @Composable () -> Color
+    val alertBackgroundColor: @Composable () -> Color
+    val oceanIcon: OceanIcons
+    val iconTint: @Composable () -> Color
+
+    class StyleInfo (
+        override val titleColor: @Composable () -> Color = { OceanColors.brandPrimaryDown },
+        override val titleStyle: @Composable () -> TextStyle = { OceanTextStyle.heading5 },
+        override val descriptionStyle: @Composable () -> TextStyle = { OceanTextStyle.caption },
+        override val descriptionColor: @Composable () -> Color = { OceanColors.interfaceDarkDown },
+        override val alertBackgroundColor: @Composable () -> Color = { OceanColors.interfaceLightUp },
+        override val oceanIcon: OceanIcons = OceanIcons.INFO_OUTLINE,
+        override val iconTint: @Composable () -> Color = { OceanColors.brandPrimaryDown }
+    ) : AlertStyle
+
+    class StyleWarning (
+        override val titleColor: @Composable () -> Color = { OceanColors.statusWarningDeep },
+        override val titleStyle: @Composable () -> TextStyle = { OceanTextStyle.heading5 },
+        override val descriptionStyle: @Composable () -> TextStyle = { OceanTextStyle.caption },
+        override val descriptionColor: @Composable () -> Color = { OceanColors.interfaceDarkDown },
+        override val alertBackgroundColor: @Composable () -> Color = { OceanColors.statusWarningUp },
+        override val oceanIcon: OceanIcons = OceanIcons.EXCLAMATION_CIRCLE_OUTLINE,
+        override val iconTint: @Composable () -> Color = { OceanColors.statusWarningDeep }
+    ) : AlertStyle
+
+    class StylePositive (
+        override val titleColor: @Composable () -> Color = { OceanColors.statusPositiveDeep },
+        override val titleStyle: @Composable () -> TextStyle = { OceanTextStyle.heading5 },
+        override val descriptionStyle: @Composable () -> TextStyle = { OceanTextStyle.caption },
+        override val descriptionColor: @Composable () -> Color = { OceanColors.interfaceDarkDown },
+        override val alertBackgroundColor: @Composable () -> Color = { OceanColors.statusPositiveUp },
+        override val oceanIcon: OceanIcons = OceanIcons.CHECK_CIRCLE_OUTLINE,
+        override val iconTint: @Composable () -> Color = { OceanColors.statusPositiveDeep }
+    ) : AlertStyle
+
+    class StyleNegative (
+        override val titleColor: @Composable () -> Color = { OceanColors.statusNegativePure },
+        override val titleStyle: @Composable () -> TextStyle = { OceanTextStyle.heading5 },
+        override val descriptionStyle: @Composable () -> TextStyle = { OceanTextStyle.caption },
+        override val descriptionColor: @Composable () -> Color = { OceanColors.interfaceDarkDown },
+        override val alertBackgroundColor: @Composable () -> Color = { OceanColors.statusNegativeUp },
+        override val oceanIcon: OceanIcons = OceanIcons.X_CIRCLE_OUTLINE,
+        override val iconTint: @Composable () -> Color = { OceanColors.statusNegativePure }
+    ) : AlertStyle
+}

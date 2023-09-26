bf9fee50a0684223c791a508a022a33ff6857103
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-09-26 19:17:04 -0300

    fix: copilot made a mistake (and i didn't see it)

--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -5,7 +5,7 @@ object Configs {
     const val targetSdkVersion = 33
     const val kotlinCompilerExtensionVersion = "1.4.8"
     const val gprBaseUrl = "https://maven.pkg.github.com"
-    const val gprRepoOwner = "useblu"
+    const val gprRepoOwner = "ocean-ds"
     const val gprRepoId = "ocean-android"
     val gprUser by lazy { System.getenv("GPR_USER") }
     val gprApiKey by lazy { System.getenv("GPR_API_KEY") }

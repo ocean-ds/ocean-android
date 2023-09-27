f60a98257896bbce889f39c1efad39d3c535fb6a
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-09-27 11:52:17 -0300

    feat: centralize repositories on settings.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,29 +2,11 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        maven { setUrl("https://jitpack.io") }
     }
 
     dependencies {
         classpath(classpath.gradle)
         classpath(classpath.kotlin)
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        maven { setUrl("https://jitpack.io") }
-        maven { setUrl("https://jcenter.bintray.com") }
-        maven {
-            name = "github-packages-maven-repo"
-            url = uri("${Configs.gprBaseUrl}/${Configs.gprRepoOwner}/${Configs.gprRepoId}")
-
-            credentials {
-                username = Configs.gprUser
-                password = Configs.gprApiKey
-            }
-        }
     }
 }
 

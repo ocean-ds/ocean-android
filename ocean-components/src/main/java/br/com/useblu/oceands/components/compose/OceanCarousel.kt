8cfb499f4116f143a1588851eb6b95bf9fe2d342
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-10-11 16:48:33 -0300

    feat: add placeholder and error fallback

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanCarousel.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanCarousel.kt
@@ -1,6 +1,7 @@
 package br.com.useblu.oceands.components.compose
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -15,11 +16,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import br.com.useblu.oceands.R
 import br.com.useblu.oceands.model.OceanCarouselItem
 import br.com.useblu.oceands.ui.compose.OceanColors
 import br.com.useblu.oceands.ui.compose.OceanSpacing
@@ -30,7 +32,7 @@ import kotlinx.coroutines.delay
 @Preview
 @Composable
 fun OceanCarouselPreview() {
-    val tucano = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCYLQmqlADnQuH6Of-4GBuuwXrZj16UEM2Bqu_faLa-Q&s"
+    val tucano = "https://ecrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCYLQmqlADnQuH6Of-4GBuuwXrZj16UEM2Bqu_faLa-Q&s"
     val carouselItems = listOf(
         OceanCarouselItem(tucano) {
             println("tucano 1")
@@ -91,10 +93,17 @@ fun OceanCarousel(
         ) {
             if (LocalInspectionMode.current) {
                 Box(
-                    modifier = Modifier.background(Color.Blue)
+                    modifier = Modifier
                         .fillMaxSize()
                         .clickable { items[it].action() }
-                )
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.image_placeholder),
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.FillBounds
+                    )
+                }
             } else {
                 GlideImage(
                     imageModel = { items[it].url },
@@ -102,7 +111,15 @@ fun OceanCarousel(
                         contentScale = ContentScale.Crop,
                         alignment = Alignment.Center
                     ),
-                    modifier = Modifier.clickable { items[it].action() }
+                    modifier = Modifier.clickable { items[it].action() },
+                    failure = {
+                        Image(
+                            painter = painterResource(id = R.drawable.image_placeholder),
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxSize(),
+                            contentScale = ContentScale.FillBounds
+                        )
+                    }
                 )
             }
         }

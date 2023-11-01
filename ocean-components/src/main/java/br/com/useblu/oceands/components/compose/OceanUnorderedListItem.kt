2da733ca65318a8f3a84d302bd98dc0dbbecf814
Tarik (Julio Martins) <38379044+nordael@users.noreply.github.com>
2023-11-01 13:56:53 -0300

    feat: adding support to html string format

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanUnorderedListItem.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanUnorderedListItem.kt
@@ -1,5 +1,6 @@
 package br.com.useblu.oceands.components.compose
 
+import android.widget.TextView
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -14,11 +15,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
 import br.com.useblu.oceands.extensions.compose.iconContainerBackground
-import br.com.useblu.oceands.utils.OceanIcons
 import br.com.useblu.oceands.model.compose.OceanUnorderedListItemModel
 import br.com.useblu.oceands.ui.compose.OceanColors
 import br.com.useblu.oceands.ui.compose.OceanTextStyle
+import br.com.useblu.oceands.utils.OceanIcons
 
 
 @Preview(showBackground = true)
@@ -38,6 +41,11 @@ fun OceanUnorderedListItemPreview() {
         OceanUnorderedListItemModel(
             title = "Mais segurança: aprove, na hora, suas transações feitas pelo app",
             style = OceanTextStyle.heading5,
+            iconType = OceanIcons.SHIELD_CHECK_OUTLINE,
+            showIconBackground = false
+        ),
+        OceanUnorderedListItemModel(
+            title = "<b>Mais segurança:</b> aprove, na hora, suas transações feitas pelo app",
             iconType = OceanIcons.SHIELD_CHECK_OUTLINE,
             showIconBackground = false
         )
@@ -102,12 +110,39 @@ fun OceanUnorderedListItem(
             )
         }
 
-        Text(
-            text = title,
-            style = style,
-            modifier = Modifier
-                .align(Alignment.Top)
-                .padding(start = 8.dp)
-        )
+        if(!isHtmlString(title)){
+            Text(
+                text = title,
+                style = style,
+                modifier = Modifier
+                    .align(Alignment.Top)
+                    .padding(start = 8.dp)
+            )
+        } else {
+            HtmlText(
+                html = title,
+                modifier = Modifier
+                    .align(Alignment.Top)
+                    .padding(start = 8.dp)
+            )
+        }
     }
+}
+
+@Composable
+fun isHtmlString(input: String): Boolean {
+    // Check if the string contains HTML tags
+    return input.contains(Regex("<[^>]*>"))
+}
+
+@Composable
+fun HtmlText(
+    modifier: Modifier = Modifier,
+    html: String,
+) {
+    AndroidView(
+        modifier = modifier,
+        factory = { context -> TextView(context) },
+        update = { it.text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_COMPACT) }
+    )
 }

6ad6be9d6bda1cc0ddf5231f06391d4a8f028057
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-11-28 15:27:09 -0300

    feat: create OceanSectionHeader

---
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanSectionHeader.kt
@@ -0,0 +1,45 @@
+package br.com.useblu.oceands.components.compose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.useblu.oceands.ui.compose.OceanColors
+import br.com.useblu.oceands.ui.compose.OceanTextStyle
+
+@Preview
+@Composable
+private fun OceanSectionHeaderPreview() {
+    Column(
+        modifier = Modifier
+            .background(color = Color(0xFFFCFCFC))
+    ) {
+        OceanSectionHeader(title = "Title", modifier = Modifier.fillMaxWidth())
+    }
+}
+
+@Composable
+fun OceanSectionHeader(
+    modifier: Modifier = Modifier,
+    title: String
+) {
+    Row(
+        modifier = modifier
+            .height(20.dp)
+    ) {
+        OceanText(
+            text = title,
+            modifier = Modifier
+                .padding(horizontal = 16.dp),
+            style = OceanTextStyle.heading5,
+            color = OceanColors.interfaceDarkUp
+        )
+    }
+}

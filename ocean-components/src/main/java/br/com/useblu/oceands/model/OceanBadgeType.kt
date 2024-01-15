40a81d5862aeaf27d7e86336bf77dcd642191a96
Fernando Zanutto <ferzanutto1999@gmail.com>
2024-01-15 15:53:09 -0300

    feat: add OceanBadgeType fromString and add parater to OceanStatusListItem

--- ocean-components/src/main/java/br/com/useblu/oceands/model/OceanBadgeType.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/model/OceanBadgeType.kt
@@ -1,8 +1,22 @@
 package br.com.useblu.oceands.model
 
-import androidx.compose.runtime.Immutable
-
-@Immutable
 enum class OceanBadgeType {
-    HIGHLIGHT, PRIMARY, PRIMARY_INVERTED, WARNING, DISABLED, CHIP_HOVER
+    HIGHLIGHT,
+    PRIMARY,
+    PRIMARY_INVERTED,
+    WARNING,
+    DISABLED,
+    CHIP_HOVER;
+
+    fun fromString(value: String): OceanBadgeType? {
+        return when (value.lowercase()) {
+            "highlight" -> HIGHLIGHT
+            "primary" -> PRIMARY
+            "primaryinverted" -> PRIMARY_INVERTED
+            "warning" -> WARNING
+            "disabled" -> DISABLED
+            "chiphover" -> CHIP_HOVER
+            else -> null
+        }
+    }
 }

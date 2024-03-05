61cc127fc4be841d426e1a1c989f712b72ac6040
Fernando Zanutto <ferzanutto1999@gmail.com>
2024-03-05 13:36:37 -0300

    feat: add tooltip do OceanChartCard and click callback

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/chart/OceanChartCard.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/chart/OceanChartCard.kt
@@ -2,6 +2,7 @@ package br.com.useblu.oceands.components.compose.chart
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,16 +15,20 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import br.com.useblu.oceands.R
 import br.com.useblu.oceands.components.compose.OceanDivider
 import br.com.useblu.oceands.components.compose.OceanGroupCta
 import br.com.useblu.oceands.components.compose.OceanText
+import br.com.useblu.oceands.components.compose.OceanTooltip
 import br.com.useblu.oceands.model.OceanDonutItem
 import br.com.useblu.oceands.model.OceanDonutModel
 import br.com.useblu.oceands.ui.compose.OceanColors
@@ -110,13 +115,27 @@ fun OceanChardLegend(
         modifier
     ) {
         model.items.forEach {
-            OceanChartLegendItem(model = it)
+            OceanChartLegendItem(
+                model = it,
+                onClick = {
+                    if (model.items.all { it.selected }) {
+                        model.onItemSelected(it)
+                    } else {
+                        if (it.selected) {
+                            model.onNothingSelected()
+                        } else {
+                            model.onItemSelected(it)
+                        }
+                    }
+                }
+            )
         }
     }
 }
 
 @Composable
 fun OceanChartLegendItem(
+    onClick: () -> Unit,
     model: OceanDonutItem
 ) {
     val alpha = if (model.selected) 1f else 0.4f
@@ -125,6 +144,9 @@ fun OceanChartLegendItem(
         modifier = Modifier
             .alpha(alpha)
             .fillMaxWidth()
+            .clickable {
+                onClick()
+            }
             .padding(vertical = 8.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
@@ -148,7 +170,16 @@ fun OceanChartLegendItem(
 
                 OceanSpacing.StackXXXS()
 
-                // OceanTooltip
+                if (model.information.isNotBlank()) {
+                    OceanTooltip(model.information) {
+                        Icon(
+                            modifier = Modifier.size(20.dp),
+                            painter = painterResource(id = R.drawable.ocean_icon_info_solid),
+                            tint = OceanColors.interfaceLightDeep,
+                            contentDescription = null
+                        )
+                    }
+                }
             }
 
             OceanSpacing.StackXXXS()

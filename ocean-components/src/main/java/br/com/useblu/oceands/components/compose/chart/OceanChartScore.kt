ec710fbf23b3c2b9af5b848221879f2efd3ff6bb
Mario Feles dos Santos Junior <mario_feles@live.com>
2024-03-06 18:12:34 -0300

    Feat/ocean chart score (#572)
    * feat: implements component ocean chart score
    
    * feat: adjust component

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/chart/OceanChartScore.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/chart/OceanChartScore.kt
@@ -1,16 +1,18 @@
 package br.com.useblu.oceands.components.compose.chart
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -21,6 +23,7 @@ import br.com.useblu.oceands.components.compose.OceanText
 import br.com.useblu.oceands.ui.compose.OceanColors
 import br.com.useblu.oceands.ui.compose.OceanSpacing
 import br.com.useblu.oceands.ui.compose.OceanTextStyle
+import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
@@ -33,14 +36,106 @@ private fun OceanChartScorePreview() {
     Column(
         modifier = Modifier
             .background(OceanColors.interfaceLightPure)
+            .verticalScroll(rememberScrollState())
             .padding(16.dp),
     ) {
 
         OceanChartScore(
             title = "Title",
             description = "Description",
+            value = 100f
+        )
+
+        OceanSpacing.StackXL()
+
+
+        OceanChartScore(
+            title = "Title",
+            description = "Description",
+            value = 200f
+        )
+
+        OceanSpacing.StackXL()
+
+        OceanChartScore(
+            title = "Title",
+            description = "Description",
+            value = 300f
+        )
+
+        OceanSpacing.StackXL()
+
+        OceanChartScore(
+            title = "Title",
+            description = "Description",
+            value = 301f
+        )
+
+        OceanSpacing.StackXL()
+
+        OceanChartScore(
+            title = "Title",
+            description = "Description",
+            value = 400f
+        )
+
+        OceanSpacing.StackXL()
+
+        OceanChartScore(
+            title = "Title",
+            description = "Description",
             value = 500f
         )
+
+        OceanSpacing.StackXL()
+
+        OceanChartScore(
+            title = "Title",
+            description = "Description",
+            value = 600f
+        )
+
+        OceanSpacing.StackXL()
+
+        OceanChartScore(
+            title = "Title",
+            description = "Description",
+            value = 700f
+        )
+
+        OceanSpacing.StackXL()
+
+        OceanChartScore(
+            title = "Title",
+            description = "Description",
+            value = 800f
+        )
+
+        OceanSpacing.StackXL()
+
+        OceanChartScore(
+            title = "Title",
+            description = "Description",
+            value = 900f
+        )
+
+        OceanSpacing.StackXL()
+
+        OceanChartScore(
+            title = "Title",
+            description = "Description",
+            value = 1000f
+        )
+
+        OceanSpacing.StackXL()
+
+        OceanChartScore(
+            title = "Title",
+            description = "Description",
+            value = 500f,
+            minValue = 300f
+        )
+
     }
 }
 
@@ -49,7 +144,10 @@ fun OceanChartScore(
     modifier: Modifier = Modifier,
     title: String,
     description: String,
-    value: Float = 0f
+    value: Float = 0f,
+    minValue: Float = 0f,
+    maxValue: Float = 1000f,
+    showAnimation: Boolean = false
 ) {
 
     Column(
@@ -79,7 +177,10 @@ fun OceanChartScore(
             factory = ::PieChart,
             update = {
                 it.setupChartScore(
-                    value = value
+                    value = value,
+                    showAnimation = showAnimation,
+                    minValue = minValue,
+                    maxValue = maxValue
                 )
             }
         )
@@ -89,17 +190,21 @@ fun OceanChartScore(
                 .fillMaxWidth()
                 .padding(horizontal = 90.dp)
                 .offset(y = (-85).dp),
-            horizontalArrangement = Arrangement.SpaceBetween
         ) {
             OceanText(
-                modifier = Modifier.padding(start = 18.dp),
-                text = "0",
+                modifier = Modifier.weight(1f),
+                text = minValue.toInt().toString(),
+                textAlign = TextAlign.Center,
                 style = OceanTextStyle.caption,
                 color = OceanColors.interfaceDarkDown
             )
+
+            OceanSpacing.StackXXXL()
+
             OceanText(
-                modifier = Modifier.padding(end = 6.dp),
-                text = "1000",
+                modifier = Modifier.weight(1f),
+                textAlign = TextAlign.Center,
+                text = maxValue.toInt().toString(),
                 style = OceanTextStyle.caption,
                 color = OceanColors.interfaceDarkDown
             )
@@ -108,7 +213,10 @@ fun OceanChartScore(
 }
 
 fun PieChart.setupChartScore(
-    value: Float
+    showAnimation: Boolean,
+    value: Float,
+    minValue: Float,
+    maxValue: Float
 ) {
     description.isEnabled = false
     legend.isEnabled = false
@@ -122,15 +230,26 @@ fun PieChart.setupChartScore(
     rotationAngle = 180f
     isRotationEnabled = false
 
+    if (showAnimation) {
+        animateY(1500, Easing.EaseInOutQuad)
+    }
+
+    var score = 0.0f
+    val scoreRemainder = maxValue - value
+
+    if (value > minValue) {
+        score = value - minValue
+    }
+
     setDrawCenterText(true)
     centerText = getCenterTextStyled(
-        title = value.toInt().toString(),
+        title = if (value > minValue) value.toInt().toString() else "",
         label = ""
     )
 
     data = PieData().apply {
-        val pieEntryScore = PieEntry(value)
-        val pieEntryRemainder = PieEntry(1000f - value)
+        val pieEntryScore = PieEntry(score)
+        val pieEntryRemainder = PieEntry(scoreRemainder)
 
         val pieEntries = listOf(
             pieEntryScore,
@@ -155,14 +274,22 @@ fun PieChart.setupChartScore(
 private fun PieChart.getColorScore(value: Float): Int {
     val colorId = when (value) {
         in 0f..300f -> {
-            R.color.ocean_color_status_negative_pure
+            R.color.ocean_color_interface_light_deep
         }
 
         in 301f..500f -> {
-            R.color.ocean_color_status_warning_deep
+            R.color.ocean_color_status_negative_deep
         }
 
-        in 501f..700f -> {
+        in 501f..600f -> {
+            R.color.ocean_color_status_negative_pure
+        }
+
+        in 601f..700f -> {
+            R.color.ocean_color_status_warning_pure
+        }
+
+        in 701f..900f -> {
             R.color.ocean_color_status_positive_pure
         }
 

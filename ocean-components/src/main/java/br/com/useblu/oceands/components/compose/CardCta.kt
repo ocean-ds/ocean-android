2894c9ab5d06e1fece95477e0b704858b1fa6a52
Fernando Zanutto <ferzanutto1999@gmail.com>
2024-03-19 13:58:17 -0300

    feat: removeunused annotation

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/CardCta.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/CardCta.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -45,7 +44,6 @@ private fun CardCtaPreview() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CardCta(
     showProgress: Boolean,

5aa99eaecd5321c40931d76c7340530aee843fd0
Fernando Zanutto <ferzanutto1999@gmail.com>
2024-04-10 12:55:03 -0300

    feat: update OceanUnorderedList to use Column

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/list/OceanUnorderedList.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/list/OceanUnorderedList.kt
@@ -6,8 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -60,11 +58,11 @@ fun OceanUnorderedList(
     models: List<OceanUnorderedListItemModel>,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(
+    Column(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        items(models) { item ->
+        models.forEach { item ->
             OceanUnorderedListItem(item)
         }
     }

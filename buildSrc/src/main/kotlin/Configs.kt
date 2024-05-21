bbf2ac325bd81449873df9cd71713a6687cff86e
Fernando Zanutto <fernando.zanutto@useblu.com.br>
2024-05-21 14:56:47 -0300

    build: fix build with kotlin 2.0.0

--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -2,7 +2,6 @@ object Configs {
     const val compileSdkVersion = 34
     const val minSdkVersion = 23
     const val targetSdkVersion = 34
-    const val kotlinCompilerExtensionVersion = "1.5.14"
     const val gprBaseUrl = "https://maven.pkg.github.com"
     const val gprRepoOwner = "ocean-ds"
     const val gprRepoId = "ocean-android"

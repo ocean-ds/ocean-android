59234fcddf86280528509e7018ae07741219099e
Fernando Zanutto <fernando.zanutto@useblu.com.br>
2024-05-28 10:44:01 -0300

    feat: create file

---
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanSimpleBalance.kt
@@ -0,0 +1,24 @@
+package br.com.useblu.oceands.components.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun OceanSimpleBalance(
+    modifier: Modifier = Modifier,
+    headerTitle: String,
+    firstLabel: String,
+    firstValue: String,
+    secondLabel: String,
+    secondValue: String,
+    thirdLabel: String,
+    thirdValue: String,
+    fourthLabel: String,
+    fourthValue: String,
+    clickIcon: () -> Unit,
+    isContentHidden: Boolean,
+    visibleShadow: Boolean,
+    hide: Boolean
+) {
+    
+}

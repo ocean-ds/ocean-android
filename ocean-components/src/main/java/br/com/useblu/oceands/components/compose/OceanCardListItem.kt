c3a5a2f5063c60e7267bd75ac445a6e329557122
Fernando Zanutto <fernando.zanutto@useblu.com.br>
2024-06-04 09:54:54 -0300

    refactor

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanCardListItem.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanCardListItem.kt
@@ -1,8 +1,7 @@
 package br.com.useblu.oceands.components.compose
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,6 +11,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -30,130 +31,132 @@ import br.com.useblu.oceands.utils.OceanIcons
 @Preview(device = "spec:width=800dp,height=550.9dp,dpi=440")
 @Composable
 private fun OceanCardListItemPreview() {
-    Row(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
-            .padding(15.dp),
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Column(
+    OceanTheme {
+        Row(
             modifier = Modifier
-                .width(250.dp)
+                .fillMaxSize()
+                .background(Color.White)
+                .padding(15.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            OceanCardListItem(
-                title = "Title",
-                onClick = {}
-            )
+            Column(
+                modifier = Modifier
+                    .width(250.dp)
+            ) {
+                OceanCardListItem(
+                    title = "Title",
+                    onClick = {}
+                )
 
-            OceanSpacing.StackXXS()
+                OceanSpacing.StackXXS()
 
-            OceanCardListItem(
-                title = "Title",
-                description = "Description"
-            )
+                OceanCardListItem(
+                    title = "Title",
+                    description = "Description"
+                )
 
-            OceanSpacing.StackXXS()
+                OceanSpacing.StackXXS()
 
-            OceanCardListItem(
-                title = "Title",
-                description = "Description",
-                caption = "Caption"
-            )
-        }
+                OceanCardListItem(
+                    title = "Title",
+                    description = "Description",
+                    caption = "Caption"
+                )
+            }
 
-        Column(
-            modifier = Modifier
-                .width(250.dp)
-        ) {
-            OceanCardListItem(
-                title = "Title",
-                leadingIconToken = OceanIcons.SWITCH_HORIZONTAL_OUTLINE
-            )
+            Column(
+                modifier = Modifier
+                    .width(250.dp)
+            ) {
+                OceanCardListItem(
+                    title = "Title",
+                    leadingIconToken = OceanIcons.SWITCH_HORIZONTAL_OUTLINE
+                )
 
-            OceanSpacing.StackXXS()
+                OceanSpacing.StackXXS()
 
-            OceanCardListItem(
-                title = "Title",
-                description = "Description",
-                leadingIconToken = OceanIcons.PLACEHOLDER_OUTLINE
-            )
+                OceanCardListItem(
+                    title = "Title",
+                    description = "Description",
+                    leadingIconToken = OceanIcons.PLACEHOLDER_OUTLINE
+                )
 
-            OceanSpacing.StackXXS()
+                OceanSpacing.StackXXS()
 
-            OceanCardListItem(
-                title = "Title",
-                description = "Description",
-                caption = "Caption",
-                leadingIconToken = OceanIcons.PAGBLU_OUTLINE,
-                isSelected = true
-            )
+                OceanCardListItem(
+                    title = "Title",
+                    description = "Description",
+                    caption = "Caption",
+                    leadingIconToken = OceanIcons.PAGBLU_OUTLINE,
+                    isSelected = true
+                )
 
-            OceanSpacing.StackXXS()
+                OceanSpacing.StackXXS()
 
-            OceanCardListItem(
-                title = "Title",
-                description = "Description",
-                caption = "Caption",
-                leadingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
-                disabled = true
-            )
-        }
+                OceanCardListItem(
+                    title = "Title",
+                    description = "Description",
+                    caption = "Caption",
+                    leadingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
+                    disabled = true
+                )
+            }
 
-        Column(
-            modifier = Modifier
-                .width(250.dp)
-        ) {
-            OceanCardListItem(
-                title = "Title",
-                leadingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
-                trailingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
-                showIconBackground = false
-            )
+            Column(
+                modifier = Modifier
+                    .width(250.dp)
+            ) {
+                OceanCardListItem(
+                    title = "Title",
+                    leadingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
+                    trailingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
+                    showIconBackground = false
+                )
 
-            OceanSpacing.StackXXS()
+                OceanSpacing.StackXXS()
 
-            OceanCardListItem(
-                title = "Title",
-                description = "Description",
-                leadingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
-                trailingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
-                showIconBackground = false
-            )
+                OceanCardListItem(
+                    title = "Title",
+                    description = "Description",
+                    leadingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
+                    trailingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
+                    showIconBackground = false
+                )
 
-            OceanSpacing.StackXXS()
+                OceanSpacing.StackXXS()
 
-            OceanCardListItem(
-                title = "Title",
-                description = "Description",
-                caption = "Caption",
-                leadingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
-                trailingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
-                showIconBackground = false
-            )
+                OceanCardListItem(
+                    title = "Title",
+                    description = "Description",
+                    caption = "Caption",
+                    leadingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
+                    trailingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
+                    showIconBackground = false
+                )
 
-            OceanSpacing.StackXXS()
+                OceanSpacing.StackXXS()
 
-            OceanCardListItem(
-                title = "Title",
-                description = "Description",
-                caption = "Caption",
-                leadingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
-                trailingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
-                disabled = true
-            )
+                OceanCardListItem(
+                    title = "Title",
+                    description = "Description",
+                    caption = "Caption",
+                    leadingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
+                    trailingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
+                    disabled = true
+                )
 
-            OceanSpacing.StackXXS()
+                OceanSpacing.StackXXS()
 
-            OceanCardListItem(
-                title = "Title",
-                description = "Description",
-                caption = "Caption",
-                leadingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
-                trailingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
-                tagLabel = "Ativo",
-                tagType = OceanTagType.Positive
-            )
+                OceanCardListItem(
+                    title = "Title",
+                    description = "Description",
+                    caption = "Caption",
+                    leadingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
+                    trailingIconToken = OceanIcons.PLACEHOLDER_OUTLINE,
+                    tagLabel = "Ativo",
+                    tagType = OceanTagType.Positive
+                )
+            }
         }
     }
 }
@@ -185,90 +188,88 @@ fun OceanCardListItem(
         OceanColors.brandPrimaryDown
     } else OceanColors.interfaceLightUp
 
-    Row(
-        modifier = modifier
-            .clickable(
-                enabled = onClick != null && !disabled,
-                onClick = { onClick?.invoke() }
-            )
-            .background(
-                color = backgroundColor,
-                shape = RoundedCornerShape(8.dp)
-            )
-            .border(
-                width = 1.dp,
-                color = borderColor,
-                shape = RoundedCornerShape(8.dp)
-            )
-            .padding(OceanSpacing.xs),
-        verticalAlignment = Alignment.CenterVertically
+    Card(
+        modifier = modifier,
+        colors = CardDefaults.cardColors(
+            containerColor = backgroundColor,
+            disabledContainerColor = backgroundColor
+        ),
+        shape = RoundedCornerShape(8.dp),
+        border = BorderStroke(1.dp, borderColor),
+        enabled = onClick != null && !disabled,
+        onClick = { onClick?.invoke() }
     ) {
-        if (leadingIconToken != null) {
-            val iconSize = if (showIconBackground) 24.dp else 20.dp
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(OceanSpacing.xs)
+        ) {
+            if (leadingIconToken != null) {
+                val iconSize = if (showIconBackground) 24.dp else 20.dp
 
-            Box(
-                modifier = Modifier
-                    .size(40.dp)
-                    .iconContainerBackground(showIconBackground, iconBackgroundColor)
-            ) {
-                OceanIcon(
-                    iconType = leadingIconToken,
+                Box(
                     modifier = Modifier
-                        .align(Alignment.Center)
-                        .size(iconSize),
-                    tint = when {
-                        disabled -> OceanColors.interfaceLightDeep
-                        isSelected -> OceanColors.interfaceLightPure
-                        else -> OceanColors.brandPrimaryDown
-                    }
-                )
+                        .size(40.dp)
+                        .iconContainerBackground(showIconBackground, iconBackgroundColor)
+                ) {
+                    OceanIcon(
+                        iconType = leadingIconToken,
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .size(iconSize),
+                        tint = when {
+                            disabled -> OceanColors.interfaceLightDeep
+                            isSelected -> OceanColors.interfaceLightPure
+                            else -> OceanColors.brandPrimaryDown
+                        }
+                    )
+                }
+
+                OceanSpacing.StackXS()
             }
 
-            OceanSpacing.StackXS()
-        }
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Row {
+                    Text(
+                        text = title,
+                        style = OceanTextStyle.paragraph,
+                        color = if (disabled) OceanColors.interfaceLightDeep else OceanColors.interfaceDarkPure
+                    )
 
-        Column(
-            modifier = Modifier.weight(1f)
-        ) {
-            Row {
-                Text(
-                    text = title,
-                    style = OceanTextStyle.paragraph,
-                    color = if (disabled) OceanColors.interfaceLightDeep else OceanColors.interfaceDarkPure
-                )
+                    if (!tagLabel.isNullOrBlank() && tagType != null) {
+                        OceanTag(label = tagLabel, type = tagType)
+                    }
+                }
 
-                if (!tagLabel.isNullOrBlank() && tagType != null) {
-                    OceanTag(label = tagLabel, type = tagType)
+                description?.let {
+                    Text(
+                        text = description,
+                        style = OceanTextStyle.description,
+                        color = if (disabled) OceanColors.interfaceLightDeep else Color.Unspecified
+                    )
+                }
+
+                caption?.let {
+                    OceanSpacing.StackXXXS()
+
+                    Text(
+                        text = caption,
+                        style = OceanTextStyle.caption,
+                        color = if (disabled) OceanColors.interfaceLightDeep else Color.Unspecified
+                    )
                 }
             }
 
-            description?.let {
-                Text(
-                    text = description,
-                    style = OceanTextStyle.description,
-                    color = if (disabled) OceanColors.interfaceLightDeep else Color.Unspecified
+            trailingIconToken?.let {
+                OceanSpacing.StackXS()
+
+                OceanIcon(
+                    iconType = it,
+                    modifier = Modifier.size(20.dp),
+                    tint = if (disabled) OceanColors.interfaceLightDeep else OceanColors.interfaceDarkUp
                 )
             }
-
-            caption?.let {
-                OceanSpacing.StackXXXS()
-
-                Text(
-                    text = caption,
-                    style = OceanTextStyle.caption,
-                    color = if (disabled) OceanColors.interfaceLightDeep else Color.Unspecified
-                )
-            }
-        }
-
-        trailingIconToken?.let {
-            OceanSpacing.StackXS()
-
-            OceanIcon(
-                iconType = it,
-                modifier = Modifier.size(20.dp),
-                tint = if (disabled) OceanColors.interfaceLightDeep else OceanColors.interfaceDarkUp
-            )
         }
     }
 }

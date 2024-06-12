ae244be26302e169abbc87cb117f8c7b83a071c9
Celso Farias <celso.farias@useblu.com.br>
2024-06-12 15:27:53 -0400

    feat: excluded class unnecessary

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanInvertedTextListItem.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanInvertedTextListItem.kt
@@ -58,7 +58,7 @@ private fun OceanInvertedTextListItemPreview() {
         OceanInvertedTextListItem(
             modifier = Modifier,
             title = "Title",
-            type = InvertedTextListType.DefaultWithIcon(
+            type = InvertedTextListType.Default(
                 description = "Description",
                 colorDescription = OceanColors.brandPrimaryDown,
                 icon = OceanIcons.DOCUMENT_TEXT_SOLID,
@@ -119,14 +119,32 @@ fun OceanInvertedTextListItem(
 sealed interface InvertedTextListType {
     @Composable
     fun TypeContent()
-    data class Default(val description: String) : InvertedTextListType {
+    data class Default(
+        val description: String,
+        val colorDescription: Color? = null,
+        val icon: OceanIcons? = null,
+        val colorIcon: Color? = null
+    ) : InvertedTextListType {
         @Composable
         override fun TypeContent() {
-            OceanText(
-                text = description,
-                style = OceanTextStyle.paragraph,
-                color = OceanColors.interfaceDarkPure
-            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                if (icon != null) {
+                    OceanIcon(
+                        iconType = icon,
+                        tint = colorIcon ?: OceanColors.statusWarningDeep,
+                        modifier = Modifier.size(20.dp)
+                    )
+                    OceanSpacing.StackXXXS()
+                }
+
+                OceanText(
+                    text = description,
+                    style = OceanTextStyle.paragraph,
+                    color = colorDescription ?: OceanColors.statusWarningDeep
+                )
+            }
         }
     }
 
@@ -215,35 +233,6 @@ sealed interface InvertedTextListType {
                         isSmall = true
                     )
                 }
-            }
-        }
-    }
-
-    data class DefaultWithIcon(
-        val description: String,
-        val colorDescription: Color? = null,
-        val icon: OceanIcons? = null,
-        val colorIcon: Color? = null
-    ) : InvertedTextListType {
-        @Composable
-        override fun TypeContent() {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                if (icon != null) {
-                    OceanIcon(
-                        iconType = icon,
-                        tint = colorIcon ?: OceanColors.statusWarningDeep,
-                        modifier = Modifier.size(20.dp)
-                    )
-                    OceanSpacing.StackXXXS()
-                }
-
-                OceanText(
-                    text = description,
-                    style = OceanTextStyle.paragraph,
-                    color = colorDescription ?: OceanColors.statusWarningDeep
-                )
             }
         }
     }

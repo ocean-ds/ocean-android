b759a05ff95fef906b74fd2aa27d81e2acd4a492
Fernando Zanutto <fernando.zanutto@useblu.com.br>
2024-07-19 17:46:53 -0300

    feat: close bottom sheet with backpress only if it is dismissible (#675)

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanBottomSheet.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanBottomSheet.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.ModalBottomSheetDefaults
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -241,7 +242,8 @@ fun OceanBottomSheet(
         containerColor = OceanColors.interfaceLightPure,
         sheetState = sheetState,
         onDismissRequest = { model.onDismiss() },
-        dragHandle = null
+        dragHandle = null,
+        properties =  ModalBottomSheetDefaults.properties(shouldDismissOnBackPress = model.isDismissible)
     ) {
         Row(
             modifier = Modifier

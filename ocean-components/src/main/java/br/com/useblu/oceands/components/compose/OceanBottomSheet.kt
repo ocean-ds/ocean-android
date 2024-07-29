8c27ca5e0f7580dcf875c4ad6af4f0592686801f
Fernando Zanutto <fernando.zanutto@useblu.com.br>
2024-07-29 15:57:43 -0300

    feat: add test tag to X button on Bottom Sheet

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanBottomSheet.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanBottomSheet.kt
@@ -29,6 +29,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -259,6 +260,7 @@ fun OceanBottomSheet(
         ) {
             if (model.isDismissible) {
                 IconButton(
+                    modifier = Modifier.testTag("close_modal"),
                     onClick = { model.onDismiss() },
                 ) {
                     OceanIcon(

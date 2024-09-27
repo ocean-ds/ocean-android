892d87470e2b3f03f996fcaf238455636ddd0fec
Mario Feles dos Santos Junior <mario_feles@live.com>
2024-09-27 10:43:10 -0300

    feat: add limit text

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/list/OceanTransactionListItem.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/list/OceanTransactionListItem.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.useblu.oceands.components.compose.OceanDivider
@@ -240,6 +241,8 @@ fun OceanTransactionListItem(
                     OceanSpacing.StackXXS()
                     Text(
                         text = dimmedLabel,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
                         style = OceanTextStyle.caption,
                         color = if (isDisabled) OceanColors.interfaceDarkUp else OceanColors.interfaceDarkDown
                     )

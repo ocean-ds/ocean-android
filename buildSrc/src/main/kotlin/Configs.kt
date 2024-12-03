ef8c256380a6e641fbf46eacbf1be24a30b13fad
Fernando Zanutto <fernando.zanutto@useblu.com.br>
2024-12-03 12:23:12 -0300

    chore: update android version

--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -1,7 +1,7 @@
 object Configs {
-    const val compileSdkVersion = 34
-    const val minSdkVersion = 23
-    const val targetSdkVersion = 34
+    const val compileSdkVersion = 35
+    const val minSdkVersion = 26
+    const val targetSdkVersion = 35
     const val gprBaseUrl = "https://maven.pkg.github.com"
     const val gprRepoOwner = "ocean-ds"
     const val gprRepoId = "ocean-android"

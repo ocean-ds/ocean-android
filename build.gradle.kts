69eef6153a8176023ebdb7a68dde1230e601f7fa
Fernando Zanutto <fernando.zanutto@useblu.com.br>
2025-02-20 12:07:11 -0300

    chore: add ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,4 +13,32 @@ buildscript {
 
 plugins {
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.ktlint)
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    ktlint {
+        outputToConsole = true
+        outputColorName = "RED"
+        ignoreFailures = false
+        baseline = file("baseline.xml")
+        additionalEditorconfig.set(
+            mapOf(
+                "max_line_length" to "250",
+                "ktlint_standard_function-signature" to "disabled",
+                "ij_kotlin_allow_trailing_comma" to "false",
+                "ij_kotlin_allow_trailing_comma_on_call_site" to "false",
+                "ktlint_function_signature_body_expression_wrapping" to "multiline",
+            ),
+        )
+        filter {
+            exclude("**/baseline.xml")
+        }
+    }
+}
+
+tasks.prepareKotlinBuildScriptModel {
+    finalizedBy(":addKtlintFormatGitPreCommitHook")
 }

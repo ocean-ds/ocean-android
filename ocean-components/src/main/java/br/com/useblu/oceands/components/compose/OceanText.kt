c6ca9eced126d8fe247161819aeb5b4d92f7d3fb
Gustavo de Melos Alencar <gustavo.melos@useblu.com.br>
2025-06-23 17:07:15 -0300

    Feat: Compact OceanBottomNavigation (#871)
    * feat: update pinpad + bttomnavigation spacing style
    
    * feat: update simplify up adjust size
    
    * chore: add draw
    
    * feat: add haptics delegate

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanText.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanText.kt
@@ -3,7 +3,13 @@ package br.com.useblu.oceands.components.compose
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
@@ -14,7 +20,9 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 import br.com.useblu.oceands.extensions.compose.htmlToAnnotatedString
+import br.com.useblu.oceands.ui.compose.OceanFontSize
 
 @Composable
 fun OceanText(
@@ -54,5 +62,74 @@ fun OceanText(
         minLines = minLines,
         onTextLayout = onTextLayout,
         style = style
+    )
+}
+
+@Composable
+fun OceanTextResizable(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    maxLines: Int = Int.MAX_VALUE,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    softWrap: Boolean = true,
+    style: TextStyle = LocalTextStyle.current,
+    minFontSize: TextUnit = OceanFontSize.xxxs
+) {
+    var currentFontSize by remember { mutableStateOf(style.fontSize) }
+    var lastTextLenght by remember { mutableIntStateOf(text.length) }
+    var canDraw by remember { mutableStateOf(true) }
+
+    OceanText(
+        modifier = modifier.drawWithContent { if (canDraw) drawContent() },
+        text = text,
+        style = style.copy(fontSize = currentFontSize),
+        color = color,
+        fontStyle = fontStyle,
+        fontWeight = fontWeight,
+        fontFamily = fontFamily,
+        letterSpacing = letterSpacing,
+        textDecoration = textDecoration,
+        textAlign = textAlign,
+        lineHeight = lineHeight,
+        overflow = TextOverflow.Visible,
+        softWrap = softWrap,
+        maxLines = maxLines,
+        onTextLayout = { textLayoutResult ->
+            when {
+                currentFontSize > style.fontSize -> {
+                    currentFontSize = style.fontSize
+                    lastTextLenght = text.length
+                    canDraw = true
+                }
+
+                currentFontSize < minFontSize -> {
+                    currentFontSize = minFontSize
+                    lastTextLenght = text.length
+                    canDraw = true
+                }
+
+                textLayoutResult.didOverflowWidth -> {
+                    currentFontSize = (currentFontSize.value * 0.9f).sp
+                    canDraw = false
+                }
+
+                else -> {
+                    if (text.length < lastTextLenght) {
+                        currentFontSize = (currentFontSize.value * 1.15f).sp
+                        canDraw = false
+                    } else {
+                        canDraw = true
+                    }
+                    lastTextLenght = text.length
+                }
+            }
+        }
     )
 }

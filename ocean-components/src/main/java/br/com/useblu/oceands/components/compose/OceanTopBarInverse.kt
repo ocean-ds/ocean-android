6da2c6a80893bf9bc608e9be98a6c6992fbc16cf
nikolaspsf <nikfersil@gmail.com>
2025-07-10 11:50:43 -0300

    fix: add import

--- ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanTopBarInverse.kt
+++ ocean-components/src/main/java/br/com/useblu/oceands/components/compose/OceanTopBarInverse.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.DropdownMenu

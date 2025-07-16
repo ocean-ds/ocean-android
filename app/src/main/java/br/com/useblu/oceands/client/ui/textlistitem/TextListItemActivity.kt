11514e827f3102cfe70135ed0e17dc7ab9be67b3
Gustavo de Melos Alencar <gustavo.melos@useblu.com.br>
2025-07-16 11:49:59 -0300

    feat: add inverted style (#889)

--- app/src/main/java/br/com/useblu/oceands/client/ui/textlistitem/TextListItemActivity.kt
+++ app/src/main/java/br/com/useblu/oceands/client/ui/textlistitem/TextListItemActivity.kt
@@ -1,30 +1,16 @@
 package br.com.useblu.oceands.client.ui.textlistitem
 
 import android.os.Bundle
-import android.widget.Toast
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
-import br.com.useblu.oceands.client.R
-import br.com.useblu.oceands.client.databinding.ActivityTextListItemBinding
+import br.com.useblu.oceands.components.compose.OceanTextListItemPreview
 
 class TextListItemActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityTextListItemBinding
-    private lateinit var viewModel: TextListItemViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_text_list_item)
-        binding.lifecycleOwner = this
-
-        viewModel = ViewModelProvider(this)[TextListItemViewModel::class.java]
-        binding.viewmodel = viewModel
-        initObservers()
-    }
-
-    private fun initObservers() {
-        viewModel.touchItem.observe(this) {
-            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+        setContent {
+            OceanTextListItemPreview()
         }
     }
 }
---
+++ ocean-components/src/main/java/br/com/useblu/oceands/model/OceanTextListContentStyle.kt
@@ -0,0 +1,47 @@
+package br.com.useblu.oceands.model
+
+import androidx.compose.runtime.Composable
+import br.com.useblu.oceands.ui.compose.OceanColors
+import br.com.useblu.oceands.ui.compose.OceanTextStyle
+
+sealed interface OceanTextListContentStyle {
+    data object Default : OceanTextListContentStyle
+
+    data object Inverted : OceanTextListContentStyle
+
+    @Composable
+    fun titleColor(enabled: Boolean) =
+        when (this) {
+            Default ->
+                if (enabled) OceanColors.interfaceDarkPure
+                else OceanColors.interfaceDarkUp
+            Inverted ->
+                if (enabled) OceanColors.interfaceDarkDown
+                else OceanColors.interfaceLightDeep
+        }
+
+    @Composable
+    fun descriptionColor(enabled: Boolean) =
+        when (this) {
+            Default ->
+                if (enabled) OceanColors.interfaceDarkDown
+                else OceanColors.interfaceLightDeep
+            Inverted ->
+                if (enabled) OceanColors.interfaceDarkPure
+                else OceanColors.interfaceDarkUp
+        }
+
+    @Composable
+    fun titleTextStyle() =
+        when (this) {
+            Default -> OceanTextStyle.paragraph
+            Inverted -> OceanTextStyle.description
+        }
+
+    @Composable
+    fun descriptionTextStyle() =
+        when (this) {
+            Default -> OceanTextStyle.description
+            Inverted -> OceanTextStyle.paragraph
+        }
+}

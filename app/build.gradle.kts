74388acce4e0b7828809c2334c87419cd5906a23
Vinicius Consulmagnos Romeiro <vinicius.romeiro@gmail.com>
2025-11-27 18:04:30 -0300

    feat: add publish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = Configs.minSdkVersion
         targetSdk = Configs.targetSdkVersion
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
     }
 
     buildTypes {
